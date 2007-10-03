338616dd3351dba114cc909af1a7c5bcb9ad842e
chrisk <devnull@localhost>
2007-10-03 18:35:51 +0200

    better sorting for object items in a array

--- src/scripts/array.js
+++ src/scripts/array.js
@@ -51,3 +51,16 @@ Array.prototype.sortByFieldName=function(fieldname)
     }
   }
 }
+
+Array.prototype.sortByItemProperty = function( property )
+{
+  this.sort
+  (
+    function( a, b )
+    {
+      if( a[property] < b[property] ) return -1;
+      if( a[property] > b[property] ) return +1;
+      return 0;
+    }
+  )
+}
