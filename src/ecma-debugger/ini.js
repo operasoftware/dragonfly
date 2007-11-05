4630b456e998bdf890ea6c5cc085f21a4a9f2c67
chrisk <devnull@localhost>
2007-11-05 11:21:50 +0100

    update to protocol version 2

--- src/ecma-debugger/ini.js
+++ src/ecma-debugger/ini.js
@@ -1,7 +1,7 @@
 var ini =
 {
   // all files in http-clients must be updated to the cvs version of this file
-  protocol_version: '1',
+  protocol_version: '2',
   max_frames: 100,
   debug: false
 }
