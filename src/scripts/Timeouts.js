81cd853887b3aab869313d006bcaff63e12b68d5
Rune Halvorsen <runeh@opera.com>
2008-07-21 16:44:35 +0200

    Added jsdoc

--- src/scripts/Timeouts.js
+++ src/scripts/Timeouts.js
@@ -1,17 +1,30 @@
 /**
-  * @constructor 
-  */
+ * @fileoverview
+ * timeouts class
+ */
 
+/**
+  * @constructor
+  * Convenience class for managing one, or many, timeouts. It keeps track
+  * of all timeout IDs so they can all be cleared at once when needed.
+  * 
+  */
 var Timeouts = function()
 {
   var timeouts = [];
 
+  /**
+   * schedule a timeout. This method clones the window.setTimeout method
+   */
   this.set = function()
   {
     this.clear();
     timeouts[timeouts.length] = setTimeout.apply(window, arguments);
   }
 
+  /**
+   * Clear all pending timeouts
+   */
   this.clear = function()
   {
     while(timeouts.length)
