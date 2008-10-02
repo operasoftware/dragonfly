b85262416db6e0bdf92785405eecf2151e0a931d
Chris K <chrisk@opera.com>
2008-10-02 11:34:45 +0200

    fix for Bug 331224, Update DOM when element is removed does not work

--- src/ecma-debugger/dom_data.js
+++ src/ecma-debugger/dom_data.js
@@ -71,7 +71,6 @@ var dom_data = new function()
 
   var domNodeRemovedHandler = function(event)
   {
-    // javascript:(function(){var e=document.getElementsByTagName('li')[0];e.parentNode.removeChild(e);})()
     // if the node is in the current data handle it otherwise not.
     var rt_id = event['runtime-id'], obj_id = event['object-id'];
     var node = null, i = 0, j = 0, level = 0, k = 0, view_id = '';
@@ -208,6 +207,10 @@ var dom_data = new function()
           {
             host_tabs.activeTab.addEventListener('mouseover', spotlight);
           } 
+          if(settings[settings_id].get('update-on-dom-node-inserted'))
+          {
+            host_tabs.activeTab.addEventListener('DOMNodeRemoved', domNodeRemovedHandler);
+          }
           if( !data.length )
           {
             getInitialView(data_runtime_id);
