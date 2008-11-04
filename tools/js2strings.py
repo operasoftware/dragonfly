717a4fad3e13b80d0384a90fc48ab3dddf585f2e
Rune Halvorsen <runeh@opera.com>
2008-11-04 20:47:21 +0100

    Renamed stringparsers module since it does more stuff Updated the tools that were using stringparsers made js2strings use the po generation stuff in dfstrings module

--- tools/js2strings.py
+++ tools/js2strings.py
@@ -1,34 +1,23 @@
-import re
 import sys
-
-tpl="""%(label)s=-1
-%(label)s.caption="%(string)s"
-%(label)s.scope="dragonfly"
-%(label)s.description="%(desc)s"
-"""
+import dfstrings
 
 if len(sys.argv)<2:
-    print """usage: js2strings js_string_file.js
+    print """usage: js2strings js_string_file.js [ ID_1 ID_2 ID_n ]
 
-Prints strings in english.db format to stdout
+Prints strings in english.db format to stdout. By default, entries for all
+strings are generated. If string IDs are passed as arguments, only those
+IDs are generated
 """
     sys.exit()
 
-fname = sys.argv[1]
+path = sys.argv[1]
+ids = sys.argv[2:] if len(sys.argv)>2 else []
 
-stringre = re.compile("""^\s*?/\*\s*?DESC:\s*(?P<desc>.*?)\s*\*/\n\s*ui_strings\.(?P<label>\S+)\s*=\s*(?:'(?P<squote>.*)'|"(?P<dquote>.*)");?$""", re.M);
-f = open(fname);
-s = f.read();
-f.close();
-matches = stringre.findall(s)
+strings = dfstrings.get_js_strings(path)
 
-strings = []
-for m in matches:
-    strings.append({"desc": m[0],
-                    "label": m[1],
-                    "string": m[2] or m[3]
-                    })
+if ids:
+    strings = [e for e in strings if e["jsname"] in ids]
 
-for s in strings: print tpl % s
-
+for s in strings: print dfstrings.make_po_entry(s)
+    
 
--- tools/stringdiff.py
+++ tools/stringdiff.py
@@ -1,5 +1,5 @@
 import sys
-import stringparsers
+import dfstrings
 
 
 def get_db_version(path):
@@ -16,12 +16,13 @@ Usage: stringdiff path_to_js path_to_english_db"""
 js_path = sys.argv[1]
 db_path = sys.argv[2]
 
-js_strings = stringparsers.get_js_strings(js_path)
-db_strings = [e for e in stringparsers.get_db_strings(db_path) if "dragonfly" in e["scope"]]
+js_strings = dfstrings.get_js_strings(js_path)
+db_strings = [e for e in dfstrings.get_db_strings(db_path) if "dragonfly" in e["scope"]]
 
 db_version = get_db_version(db_path)
 missing = list(set([e["jsname"] for e in js_strings]) - set([e["jsname"] for e in db_strings]))
 missing.sort()
+
 
 print """Comparing %s and %s version %s.\nFound %s missing strings""" % (js_path, db_path, db_version, len(missing) or "no")
 if missing:
