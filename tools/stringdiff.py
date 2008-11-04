4a54268017c4430f925bc7584b98ea618b51219c
Rune Halvorsen <runeh@opera.com>
2008-11-04 21:51:31 +0100

    Added proper option parsing Added two more output modes, so it can emit .db data directly

--- tools/stringdiff.py
+++ tools/stringdiff.py
@@ -1,29 +1,50 @@
 import sys
 import dfstrings
-
+import optparse
 
 def get_db_version(path):
     fp = open(path)
     for line in fp:
         if line.startswith("@dbversion"): return line.strip()[11:]
 
-if len(sys.argv) != 3:
-    print """Prints summary of differences between a dragonfly javascript string
-file and english.db to stdout.
-Usage: stringdiff path_to_js path_to_english_db"""
-    sys.exit(0)
+def main():
+    usage = """%prog [options] jspath dbpath
+    
+Find strings that are in the js file but missing in the db file"""
+    parser = optparse.OptionParser(usage)
+    parser.add_option("-b", "--bare", dest="bare",
+                      default=False, action="store_true",
+                      help="just emit the missing string IDs")
+    parser.add_option("-p", "--po", dest="po",
+                      default=False, action="store_true",
+                      help="emit missing strings in english.db format")
 
-js_path = sys.argv[1]
-db_path = sys.argv[2]
+    options, args = parser.parse_args()
+    if len(args) != 2:
+        parser.error("Need both jspath and dbpath")
+    elif options.bare and options.po:
+        parser.error("-p and -b are mutually exclusive")
+        
 
-js_strings = dfstrings.get_js_strings(js_path)
-db_strings = [e for e in dfstrings.get_db_strings(db_path) if "dragonfly" in e["scope"]]
+    js_strings = dfstrings.get_js_strings(args[0])
+    db_strings = [e for e in dfstrings.get_db_strings(args[1]) if "dragonfly" in e["scope"]]
+    db_version = get_db_version(args[1])
 
-db_version = get_db_version(db_path)
-missing = list(set([e["jsname"] for e in js_strings]) - set([e["jsname"] for e in db_strings]))
-missing.sort()
+    # the following would be nicer if we could put dictionaries in sets,
+    # but they're not hashable.
 
+    db_stringids = [e["jsname"] for e in db_strings]
+    missing = [ e for e in js_strings if not e["jsname"] in db_stringids ]
+    
+    if options.bare:
+        print " ".join([e["jsname"] for e in missing])
+    elif options.po:
+        for s in missing: print dfstrings.make_po_entry(s)
+    else:
+        print """Comparing %s and %s version %s.\nFound %s missing strings""" % (args[0], args[1], db_version, len(missing) or "no")
+        if missing:
+            for m in missing: print "\t", m["jsname"]
 
-print """Comparing %s and %s version %s.\nFound %s missing strings""" % (js_path, db_path, db_version, len(missing) or "no")
-if missing:
-    for m in missing: print "\t", m
+if __name__ == "__main__":
+    sys.exit(main())
+
