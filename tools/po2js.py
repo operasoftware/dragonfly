38b449ae239536a9a7483d8e6bb625e271144bb9
Rune Halvorsen <runeh@opera.com>
2009-01-30 07:44:26 -0500

    Made the po building tools more unicode aware. Now able to emit .js files for all languages in the translations module

--- tools/po2js.py
+++ tools/po2js.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 import sys
 import os.path
 import codecs
@@ -7,11 +10,11 @@ import time
 def make_js_from_po(path):
     strings = []
     for po in [p for p in dfstrings.get_po_strings(path) if "scope" in p and "dragonfly" in p["scope"] ]:
-        strings.append("""ui_strings.%s="%s";""" % (po["jsname"], po["msgstr"]))
+        strings.append(u"""ui_strings.%s="%s";""" % (po["jsname"], po["msgstr"]))
     return """/* Generated from %s at %s */
 window.ui_strings || ( window.ui_strings  = {} ) 
 window.ui_strings.lang_code = "?";
-%s""" % (os.path.basename(path), time.asctime(), "\n".join(strings))
+%s""" % (unicode(os.path.basename(path)), unicode(time.asctime()), u"\n".join(strings))
 
 def main():
     if len(sys.argv)==1:
@@ -23,8 +26,9 @@ def main():
             outfile = codecs.open(sys.argv[2], "w", encoding="utf_8_sig")
         else:
             outfile = sys.stdout
-            
-        outfile.write(make_js_from_po(infile))
+         
+	data = make_js_from_po(infile)
+        outfile.write(data)
         return 0
 
 if __name__ == "__main__":
