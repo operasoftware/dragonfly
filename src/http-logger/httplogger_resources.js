368ec4f54ee78f8f2c5f450d5ef35ecc6918fd95
Rune Halvorsen <runeh@opera.com>
2009-02-03 14:34:05 +0100

    Moved mime types stuff into the resources file so it's all in one place

--- src/http-logger/httplogger_resources.js
+++ src/http-logger/httplogger_resources.js
@@ -1,6 +1,7 @@
 ﻿/**
  * @fileoverview
- * Mappings between various types of http data and their relevant specs
+ * Mappings between various types of http data and their relevant specs as
+ * well as mappings and helpers used for resolving mime types
  */
 window.http_header_specification_urls = {
     // from HTTP spec
@@ -71,4 +72,62 @@ window.http_method_specification_urls = {
     "delete": "http://www.w3.org/Protocols/rfc2616/rfc2616-sec9.html#sec9.7",
     "trace": "http://www.w3.org/Protocols/rfc2616/rfc2616-sec9.html#sec9.8",
     "connect": "http://www.w3.org/Protocols/rfc2616/rfc2616-sec9.html#sec9.9"
+}
+
+function http_map_mime_to_type(mime)
+{
+    mime = mime.toLowerCase();
+
+    if (mime in http_mime_type_map)
+    {
+        return http_mime_type_map[mime];
+    }
+
+    if (mime.indexOf("image/") == 0)
+    {
+        return "image";
+    }
+
+    if (mime.indexOf("video/") == 0)
+    {
+        return "video";
+    }
+    
+    if (mime.indexOf("audio/") == 0)
+    {
+        return "audio";
+    }
+    
+    return "unknown";
+}
+
+window.http_mime_type_map = {
+    "text/html": "markup",
+    "text/css": "css",
+    "application/x-javascript": "javascript",
+    "application/xhtml+xml": "markup",
+    "application/pdf": "pdf",
+    "application/x-shockwave-flash": "flash",
+    "application/xml": "xml"
+}
+
+/**
+ * This one will be deprecated when we get mime types straight from core.
+ */
+window.http_file_extension_map = {
+    "html": "text/html", "htm": "text/html",
+    "xml": "application/xml",
+    "xhtml": "application/xhtml+xml", "xht": "application/xhtml+xml",
+    "swf": "application/x-shockwave-flash",
+    "png": "image/png",
+    "ico": "image/x-icon",
+    "gif": "image/gif",
+    "jpg": "image/jpeg", "jpeg": "image/jpeg", "jpe": "image/jpeg",
+    "css": "text/css"
+}
+
+function http_get_mime_from_extension(path)
+{
+    var ext = path.split(".").pop().toLowerCase();
+    return http_file_extension_map[ext] || "";
 }
