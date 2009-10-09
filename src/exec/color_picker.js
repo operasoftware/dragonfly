fb973c805c2f6ebe7ac80210f1639ac1e3cb16c1
Chris K <chrisk@opera.com>
2009-10-09 14:23:59 +0200

    color picker code clean up

--- src/exec/color_picker.js
+++ src/exec/color_picker.js
@@ -15,81 +15,134 @@ cls.ColorPicker = function(id, name, container_class)
   this.createView = function(container)
   {
     _container = container;
+    this.get_dimesions();
     container.innerHTML = 
       "<div class='padding'>" +
         "<h1>Color Picker</h1>" +
         "<p><label><input type='checkbox' handler='utils-color-picker'/> color picker</label></p>" +
-        "<p><img src='' style='width:11px;height:11px;border: 1px solid #999;' /></p>" +
-        "<p><canvas style='width:11px;height:11px;border: 1px solid #999;' ></canvas></p>" +
-        "<p><canvas style='width:" + ( this._x * this._scale ) + "px;height:" + ( this._y * this._scale ) + "px;border: 1px solid #999;' ></canvas></p>" +
+        "<p><select id='scale-select-container' handler='update-color-picker-scale'></select></p>" +
+        "<div id='table-container'></div>" +
         "<pre></pre>" +
       "</div>";
-    this._screenshot_element = container.getElementsByTagName('img')[0];
-    this._canvas_source = container.getElementsByTagName('canvas')[0];
-    this._canvas_source.width = 11;
-    this._canvas_source.height = 11;
-    this._ctx_source =  this._canvas_source.getContext('2d');
-    this._canvas_target = container.getElementsByTagName('canvas')[1];
-    //alert(this._canvas_target);
-    this._canvas_target.width = this._x * this._scale;
-    this._canvas_target.height = this._y * this._scale;
-    this._ctx_target = this._canvas_target.getContext('2d');
+    this._create_table();
+    this._create_scale_select();
 
   }
 
-  this._x = 11;
-  this._y = 11;
-  this._scale = 20;
+  this._width = 0;
+  this._height = 0;
+  this._scale = 30;
+
+  this.update_scale = function(scale)
+  {
+    this._scale = scale;
+    this._create_table();
+  }
+
+  this.get_dimesions = function()
+  {
+    var area = window.color_picker_data.get_dimensions();
+    this._width = area.width;
+    this._height = area.height;
+  }
+
+  this._create_scale_select = function()
+  {
+    const
+    DELTA = 5,
+    MAX_DIMENSION = 350;
+    
+    var 
+    markup = "",
+    max_scale = MAX_DIMENSION / this._width >> 0,
+    i = DELTA;
+
+    for( ; i  < max_scale ; i += DELTA)
+    {
+      markup += "<option" +  ( i == this._scale && " selected='selected'" || "" ) + ">" + i + "</option>";
+    }
+    document.getElementById('scale-select-container').innerHTML = markup;
+  }
+
+  this._create_table = function()
+  {
+    var 
+    cell = "<td style='height:" + this._scale + "px;width:" + this._scale + "px;'></td>",
+    markup = "",
+    tr = "",
+    i = 0;
+
+    for( ; i < this._width && (markup += cell); i++);
+    tr = markup = "<tr>" + markup + "</tr>";
+    for( i = 1; i < this._height && ( markup += tr ); i++);
+    document.getElementById('table-container').innerHTML = "<table>" + markup + "</table>";
+  }
 
   this.update_screenshot = function()
   {
-    var x = 0, y = 0, img_data = null, scale = this._scale;
-
-    this._screenshot_element.src = color_picker_data.get_screenshot();
-    this._ctx_target.clearRect(0, 0, this._x * scale, this._x * scale);
-    this._ctx_source.drawImage(this._screenshot_element, 0, 0, 11, 11);
-    
-    for( ; x < this._x; x++)
+    var 
+    pixel_count = this._width * this._height,
+    img_data = window.color_picker_data.get_data(), 
+    tds = document.getElementById('table-container').getElementsByTagName('td'), 
+    i = 0,
+    cur = 0;
+      
+    for( ; i < pixel_count; i++)
     {
-      for( y = 0; y < this._y; y++)
-      {
-        img_data = this._ctx_source.getImageData(x, y, 1, 1).data;
-        this._ctx_target.fillStyle = "rgb(" + img_data[0] + "," + img_data[1] + "," + img_data[2] +")";
-        this._ctx_target.fillRect(x * scale, y * scale , scale, scale);
-      }
-
+      cur = 4 * i;
+      tds[i].style.backgroundColor = "rgb(" + 
+          img_data[cur + 0] + "," + 
+          img_data[cur + 1] + "," + 
+          img_data[cur + 2] +")";
     }
-    var img_data = this._ctx_source.getImageData(0,0,1,1).data;
-    this._ctx_target.fillStyle = "rgb(" + img_data[0] + "," + img_data[1] + "," + img_data[2] +")";
-    this._ctx_target.fillRect(0,0,8,8);
-
-    // opera.postError(img_data[0]+' '+img_data[1]+' '+img_data[2]+' '+img_data[3]);//img_data.length);
-
   }
-
-
-
 
   this.init(id, name, container_class);
 }
 
 var color_picker_data = new function()
 {
+  const 
+  INTERVAL = 50,
+  INTERVAL_SLEEP = 500;
+
+
   // update values on top rt change
   this._top_rt_id = 0;
   this._color_picker = 0;
   this._color_picker_rt_id = 0;
   this._interval = 0;
   this._is_active = false;
+  this._width = 9;
+  this._height = 9;
+  this._delta = this._height / 2 << 0;
   this._x = 0;
   this._y = 0;
+  this._count_no_change = 0;
+  this._intervall = INTERVAL;
+
+  this._screenshot_element = null; //document.createElement('img'); 
+  //this._screenshot_element.onload = function(){opera.postError('screenshot loaded')};
+  this._canvas_source = null; //document.createElement('canvas'); 
+  this._ctx_source =  null; //this._canvas_source.getContext('2d');
 
   var self = this;
 
-  const 
-    INTERVAL = 100,
-    WIDTH = 11,
-    HEIGHT = 11;
+  this.get_dimensions = function()
+  {
+    return {width: this._width, height: this._height};
+  }
+
+  this.get_data = function()
+  {
+    return this._data;
+  }
+
+  this._set_canavas_dimensions = function()
+  {
+    this._canvas_source.width = this._width;
+    this._canvas_source.height = this._height;
+  }
 
   this.set_active_state = function(bool)
   {
@@ -155,7 +208,7 @@ var color_picker_data = new function()
   this.activate_color_picker = function()
   {
     this._is_active = true;
-    setTimeout(this.get_mouse_position_bound, INTERVAL);
+    setTimeout(this.get_mouse_position_bound, this._interval);
   }
 
   this.get_mouse_position = function()
@@ -174,24 +227,40 @@ var color_picker_data = new function()
 
   this.handle_mouse_position = function(xml)
   {
-    //opera.postError(888)
     var status = xml.getNodeData('status');
     if( status == 'completed' )
     {
       var return_value = xml.getElementsByTagName('string')[0];
-      var pos = eval(return_value.textContent);
-      //opera.postError(pos +' '+this._is_active)
+      var pos_raw = eval(return_value.textContent);
+      var pos = pos_raw && {
+        x: pos_raw.x - this._delta,
+          y: pos_raw.y - this._delta,
+          w: this._width,
+          h: this._height
+
+      } || null;
       if(pos === null || pos.x == this._x && pos.y == this._y)
       {
         if(this._is_active)
         {
-          setTimeout(this.get_mouse_position_bound, INTERVAL);
+          /* */
+          this._count_no_change += 1;
+          if(this._count_no_change >= 5)
+          {
+            this._interval = INTERVAL_SLEEP;
+          }
+          /* */
+          setTimeout(this.get_mouse_position_bound, this._interval);
         }
       }
       else
       {
         this._x = pos.x;
         this._y = pos.y;
+        /* */
+        this._count_no_change = 0;
+        this._interval = INTERVAL;
+        /* */
         window.services.exec.screen_watcher(this.handle_screenshot_bound, 0, 0, pos);
       }
     }
@@ -202,10 +271,12 @@ var color_picker_data = new function()
     var png= xml.getElementsByTagName('png')[0];
     if(png)
     {
-      this._screen_shot = "data:image/png;base64," + png.firstChild.nodeValue;
-      window.views.color_picker.update_screenshot();
+      this._screenshot_element.src = "data:image/png;base64," + png.firstChild.nodeValue;
     }
-    setTimeout(this.get_mouse_position_bound, INTERVAL);
+    else
+    {
+      setTimeout(this.get_mouse_position_bound, this._interval);
+    }
   }
 
   this.handle_screenshot_bound = function(xml)
@@ -213,23 +284,26 @@ var color_picker_data = new function()
     self.handle_screenshot(xml);
   }
 
-  this.get_screenshot = function()
+  this.handle_screen_shot_data = function()
   {
-  return this._screen_shot;
+    this._ctx_source.drawImage(this._screenshot_element, 0, 0, this._width, this._height);
+    this._data = this._ctx_source.getImageData(0, 0, this._width, this._height).data;
+    window.views.color_picker.update_screenshot();
+    setTimeout(this.get_mouse_position_bound, this._interval);
   }
 
+  this.handle_screen_shot_data_bound = function()
+  {
+    self.handle_screen_shot_data();
+  }
 
-
-
+  // color picker class on the host side
   var ColorPicker = function()
   {
     var mousemove_event = null;
     var mousemove_is_listening = false;
     var is_setup = false;
-    var width = 11;
-    var height = 11;
-    var delta_x = -5;
-    var delta_y = -5;
+
     var mousemove_handler = function(event)
     {
       mousemove_event = event;
@@ -254,17 +328,11 @@ var color_picker_data = new function()
       if(mousemove_event)
       {
         return "({" +
-            "x:" + (mousemove_event.pageX + delta_x) + "," +
-            "y:" + (mousemove_event.pageY + delta_y) + "," +
-            "w:" + width + "," +
-            "h:" + height + 
+            "x:" + mousemove_event.pageX + "," +
+            "y:" + mousemove_event.pageY +
           "})"
       };
       return "null";
-    };
-    this.set_area = function(area)
-    {
-      // TODO
     };
     this.setup = function()
     {
@@ -289,8 +357,21 @@ var color_picker_data = new function()
     this.setup();
   };
   
-  this["return new ColorPicer()"] = 
-    "return new (" + ColorPicker.toString() + ")()";
+
+
+  
+  this.init = function()
+  {
+    this["return new ColorPicer()"] = 
+      "return new (" + ColorPicker.toString() + ")()";
+    this._screenshot_element = document.createElement('img'); 
+    this._screenshot_element.onload = this.handle_screen_shot_data_bound;
+    this._canvas_source = document.createElement('canvas'); 
+    this._ctx_source =  this._canvas_source.getContext('2d');
+    this._set_canavas_dimensions();
+  }
+  // constructor calls
+  this.init();
 }
 
 cls.ColorPicker.prototype = ViewBase;
@@ -299,4 +380,9 @@ new cls.ColorPicker('color_picker', 'Color Picker', 'scroll');
 eventHandlers.change['utils-color-picker'] = function(event, target)
 {
   window.color_picker_data.set_active_state(target.checked);
+}
+
+eventHandlers.change["update-color-picker-scale"] = function(event, target)
+{
+  window.views.color_picker.update_scale(parseInt(target.value));
 }
