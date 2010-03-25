9096a022637cb9ba3c20d2473adc3d327bcd7963
David Håsäther <davidh@opera.com>
2010-03-25 14:32:18 +0100

    Fixed DFL-1044: Improper escaping in Properties view.

--- src/ecma-debugger/DOM_markup_style.js
+++ src/ecma-debugger/DOM_markup_style.js
@@ -188,7 +188,7 @@ var DOM_markup_style = function(id, name, container_class)
                     ( /^href|src$/i.test(attr[ATTR_KEY])
                       ? " handler='dom-resource-link'"
                       : "" ) + ">\"" + 
-                    attr[ATTR_VALUE].replace(/</g, '&lt;').replace(/"/g, '&amp;quot;') + 
+                    attr[ATTR_VALUE].replace(/</g, '&lt;').replace(/"/g, '&amp;quot;') +
                     "\"</value>";
               }
             }
