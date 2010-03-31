2ad4ee7ce6b2cdd0e858ff029bdaf563175bbebc
Chris K <chrisk@opera.com>
2010-03-31 18:47:38 +0200

    implemented submitting a new protocol and view existing protocols

--- manual-test-suite/dftests.py
+++ manual-test-suite/dftests.py
@@ -3,13 +3,17 @@ import os
 import time
 import string
 import re
+import time
 from mimetypes import types_map
+from urllib import quote, unquote
 from resources.markup import *
 
 APP_ROOT, file_name = os.path.split(os.path.abspath(__file__))
 TESTS = os.path.join(APP_ROOT, "TESTS")
+ID_COUNT = os.path.join(APP_ROOT, 'storage', 'ID_COUNT')
+IDS = os.path.join(APP_ROOT, 'storage', 'IDS')
 
-debug_log = open('debug.log', 'a')
+# debug_log = open('debug.log', 'a')
 
 if sys.platform == "win32":
     import os, msvcrt
@@ -24,106 +28,66 @@ class Entry(object):
         self.id = []
         self.buffer = []
 
-count = 0
-def get_id():
-    global count
-    count += 1
-    return "id_%s" % count
-
-def redirect_home(environ, start_response): 
-    status = "301 Moved Permanently"
-    # this looks a bit stupid, but to keep relative url's working the path must be a directory ( means i don't know better )
-    response_headers = \
-    [
-        (
-            "Location", 
-            environ["wsgi.url_scheme"] + "://" + 
-            environ["SERVER_NAME"] + 
-            ( "SERVER_PORT" in environ and ( ":" + environ["SERVER_PORT"] ) or "" ) +
-            environ["REQUEST_URI"] + "/"
-        )
-     ]
-    start_response(status, response_headers)
-    return []
-
-def check_test_index():
-    ID = 1
-    LABEL = 2
-    DESC = 3
-    ERROR = 4
-    state = DESC
-    in_file = open(TESTS, 'r')
+def get_ids():
+    f_ids = open(IDS, 'r')
+    ids = [id.strip() for id in f_ids.readlines()]
+    f_ids.close()
+    return ids
     
-    # Any entry with a label: must be followed by an id: and a desc:
-    
-    for line in in_file.readlines():
-        if line.startswith('id:'):
-            if not state == DESC:
-                state = ERROR
-                break
-            state = ID
-        elif line.startswith('label:'):
-            if not state == ID:
-                state = ERROR
-                break
-            state = LABEL
-        elif line.startswith('desc:'):
-            if not state == LABEL:
-                state = ERROR
-                break
-            state = DESC
-    in_file.close()
-    return state != ERROR
 
-def add_ids_test_index():
-    import shutil
-    import tempfile
-    ID = 1
-    LABEL = 2
-    DESC = 3
-    ERROR = 4
-    state = DESC
-    in_file = open(TESTS, 'r')
-    f_id_count = open(os.path.join(APP_ROOT, 'storage', 'ID_COUNT'), 'r')
-    id_count = int(f_id_count.readline().strip())
-    f_id_count.close()
-    tmpfd, tmppath = tempfile.mkstemp(".tmp", "dftests.")
-    tmpfile = os.fdopen(tmpfd, "w")
-    for line in in_file.readlines():
-        if line.startswith('id:'):
-            if not state == DESC:
-                state = ERROR
-                break
-            state = ID
-        elif line.startswith('label:'):
-            if state == DESC:
-                tmpfile.write("id: %s\n" % id_count)
-                id_count += 1
-                state = ID
-            if not state == ID:
-                state = ERROR
-                break
-            state = LABEL
-
-        elif line.startswith('desc:'):
-
-            if not state == LABEL:
-                state = ERROR
-                break
-            state = DESC
-        tmpfile.write(line)
-    tmpfile.close()
-    shutil.copy(tmppath, 'test-id')
-    os.unlink(tmppath)
-    f_id_count = open(os.path.join(APP_ROOT, 'storage', 'ID_COUNT'), 'w')
-    f_id_count.write(str(id_count))
-    f_id_count.close()
-    in_file.close()
-    
-def write_index():
+def parse_tests():
     in_file = open(TESTS, 'r')
     entries = []
-    def append_entry(entry):
+    entry = Entry()
+    cur = entry.buffer
+    for line in in_file.readlines():
+        line = line.strip()
+        if line.startswith('#'):
+            continue
+        elif not line:
+            entries.append(entry)
+            entry = Entry()
+            cur = entry.buffer
+        elif line.startswith('id:'):
+            cur = entry.id
+            cur.append(line[3:])
+        elif line.startswith('label:'):
+            cur = entry.label
+            cur.append(line[6:])
+        elif line.startswith('desc:'):
+            cur = entry.desc
+            cur.append(line[5:])
+        elif line.startswith('url:'):
+            cur = entry.url
+            cur.append(line[4:])
+        elif line.startswith('***'):
+            entry.title = entry.buffer
+        else:
+            cur.append(line)
+    in_file.close()
+    return entries
+
+def write_tester_changeset(missing, submitted):
+    ret = []
+    for key in ["tester", "changeset"]:
+        if key in submitted and submitted[key]:
+            ret.append(TR_VALUE_CHECK % (
+                "submitted",
+                key, 
+                key, 
+                VALUE_DISABLED % submitted[key],
+                INPUT_HIDDEN % (key, submitted[key])
+            ))
+        else:
+            ret.append(TR_VALUE_CHECK % ("missing", key, key, "", ""))
+
+    return "".join(ret)
+    
+
+def write_index_missing(missing, submitted):
+    entries = []
+    tests = parse_tests()
+    for entry in tests:
         if entry.title:
             title = "".join(entry.title)
             entries.append(TR_TD_COLS_4 % title)
@@ -131,40 +95,89 @@ def write_index():
             url = "".join(entry.url)
             entries.append(TR_TD_URL % (url, url))
         if entry.label:
-            id = get_id()
+            id = "id-%s" % entry.id[0].strip()
             label = "".join(entry.label)
-            desc = [""]
-            desc.extend([item.replace("\"", "&quot;") for item in re.split(" ?- *|\* *", " ".join(entry.desc)) if item])
-            desc.append("")
-            entries.append(TR_TEST % ("\n    ".join(desc), label, id, id))
-            
-    entry = Entry()
-    cur = entry.buffer
-    for line in in_file.readlines():
-        if line.startswith('#'):
-            continue
-        elif not line.strip():
-            append_entry(entry)
-            entry = Entry()
-            cur = entry.buffer
-        elif line.startswith('id:'):
-            cur = entry.id
-            cur.append(line[3:].strip())
-        elif line.startswith('label:'):
-            cur = entry.label
-            cur.append(line[6:].strip())
-        elif line.startswith('desc:'):
-            cur = entry.desc
-            cur.append(line[5:].strip())
-        elif line.startswith('url:'):
-            cur = entry.url
-            cur.append(line[4:].strip())
-        elif line.startswith('***'):
-            entry.title = entry.buffer
-        else:
-            cur.append(line.strip())
-    in_file.close()
+            desc = "\n    ".join(
+                [""] + 
+                [item.strip(' -*').replace("\"", "&quot;") 
+                            for item in entry.desc if item] +
+                [""]
+            )
+            if id in missing:
+                tr = TR_TEST_CHECK % ("missing", desc, label, id, "", id, "", "")
+            else:
+                tr = TR_TEST_CHECK % ("submitted", desc, label,
+                    id,
+                    (submitted[id] == "PASS" and " checked" or "") + " disabled",
+                    id,
+                    (submitted[id] == "FAIL" and " checked" or "") + " disabled",
+                    INPUT_HIDDEN % (id, submitted[id])
+                )
+            entries.append(tr)
     return entries
+
+def write_index():
+    entries = []
+    for entry in parse_tests():
+        if entry.title:
+            title = "".join(entry.title)
+            entries.append(TR_TD_COLS_4 % title)
+        if entry.url:
+            url = "".join(entry.url)
+            entries.append(TR_TD_URL % (url, url))
+        if entry.label:
+            id = "id-%s" % entry.id[0].strip()
+            label = "".join(entry.label)
+            desc = "\n    ".join(
+                [""] + 
+                [item.strip(' -*').replace("\"", "&quot;") 
+                            for item in entry.desc if item] +
+                [""]
+            )   
+            entries.append(TR_TEST % (desc, label, id, id))
+    return entries
+
+def parse_protocol(path):
+    f = open(path, 'r')
+    lines = f.readlines()
+    f.close
+    return dict((line.strip().split(': ') for line in lines if line.strip()))
+
+
+def write_protocol(protocol):
+    entries = []
+    for entry in parse_tests():
+        if entry.title:
+            title = "".join(entry.title)
+            entries.append(TR_TD_COLS_4 % title)
+        if entry.url:
+            url = "".join(entry.url)
+            entries.append(TR_TD_URL % (url, url))
+        if entry.label:
+            id = "id-%s" % entry.id[0].strip()
+            label = "".join(entry.label)
+            class_name = protocol[id] == "PASS" and "pass" or "fail"
+            desc = "\n    ".join(
+                [""] + 
+                [item.strip(' -*').replace("\"", "&quot;") 
+                            for item in entry.desc if item] +
+                [""]
+            )   
+            entries.append(TR_TEST_PROTOCOL % (desc, class_name, label, protocol[id]))
+    return entries
+
+
+def redirect_with_trilling_slash(environ, start_response):  
+    status = "301 Moved Permanently"
+    url = "".join([
+        environ["wsgi.url_scheme"],
+        "://",
+        environ["SERVER_NAME"], 
+        ("SERVER_PORT" in environ and ":" + environ["SERVER_PORT"] or ""),
+        environ["REQUEST_URI"],
+        "/"])
+    start_response(status, [("Location", url)])
+    return []
             
 def serve_test_form(environ, start_response):
     status = '200 OK'
@@ -172,26 +185,40 @@ def serve_test_form(environ, start_response):
     start_response(status, response_headers)
     if not check_test_index():
         add_ids_test_index()
-    doc = [TEST_FORM]
+    script_repo = environ['SCRIPT_NAME'][0:environ['SCRIPT_NAME'].rfind("/")]
+    doc = [TEST_FORM % (script_repo, script_repo, "", FORM), TR_TESTER_AND_CHANGESET_FORM]
     doc.extend(write_index())
     doc.append(TEST_FORM_END)
-
     return doc
 
 def submit_form(environ, start_response):
+    raw_content = environ["wsgi.input"].read()
+    submitted = dict([item.split('=') for item in raw_content.split('&')])
+    missing = filter(lambda id: not id in submitted or not submitted[id], get_ids())
     status = '200 OK'
-    response_headers = [('Content-type', 'text/plain')]
+    response_headers = [('Content-type', 'text/html')]
     start_response(status, response_headers)
-    raw_conten = environ["wsgi.input"].read()
-    doc = [raw_conten]
+    script_repo = environ['SCRIPT_NAME'][0:environ['SCRIPT_NAME'].rfind("/")]
+    if missing:
+        doc = [TEST_FORM % (script_repo, script_repo, LEGEND_MISSING, FORM), 
+                    write_tester_changeset(missing, submitted)]
+        doc.extend(write_index_missing(missing, submitted))
+        doc.append(TEST_FORM_END)
+    else:
+        protocol = parse_protocol(store_protocol(submitted))
+        doc = [
+            TEST_FORM % (script_repo, script_repo, "", ""), 
+            TR_TESTER_AND_CHANGESET % (submitted["tester"], submitted["changeset"])
+            ]
+        doc.extend(write_protocol(protocol))
+        doc.append(TEST_END)
     return doc
 
 def serve_index(environ, start_response):
     status = '200 OK'
     response_headers = [('Content-type', 'text/html')]
     start_response(status, response_headers)
-    doc = [INDEX]
-    return doc
+    return [INDEX]
 
 def serve_resource(environ, start_response):
     path = environ['PATH_INFO']
@@ -213,6 +240,40 @@ def serve_resource(environ, start_response):
     start_response(status, response_headers)
     return [content]
 
+def list_protocols(protocols):
+    ret = []
+    for f_name in protocols:
+        ret.append(LIST_LINK % ("./show_protocol/" + f_name, f_name))
+    return "".join(ret)
+
+def serve_protocols(environ, start_response):
+    status = '200 OK'
+    response_headers = [('Content-type', 'text/html')]
+    start_response(status, response_headers)
+    repo = os.path.join(APP_ROOT, 'storage')
+    protocols = [f_name for f_name in os.listdir(repo) if f_name.endswith('.protocol')]
+
+    if not check_test_index():
+        add_ids_test_index()
+    doc = [PROTOCOL_LIST % list_protocols(protocols)]
+    return doc
+
+def serve_protocol(environ, start_response):
+    status = '200 OK'
+    response_headers = [('Content-type', 'text/html')]
+    start_response(status, response_headers)
+    protocol_path = os.path.join(APP_ROOT, 'storage', environ['PATH_INFO'].split("/")[2])
+    protocol = parse_protocol(protocol_path)
+    script_repo = environ['SCRIPT_NAME'][0:environ['SCRIPT_NAME'].rfind("/")]
+    doc = [
+        TEST_FORM % (script_repo, script_repo, "", ""), 
+        TR_TESTER_AND_CHANGESET % (protocol["tester"], protocol["changeset"])
+        ]
+    doc.extend(write_protocol(protocol))
+    doc.append(TEST_END)
+    return doc
+
+
 def not_supported(environ, start_response):
     status = '200 OK'
     response_headers = [('Content-type', 'text/plain')]
@@ -222,21 +283,109 @@ def not_supported(environ, start_response):
 handlers = {
     "/": serve_index,
     "/test-form": serve_test_form,
-    "/submit-form": submit_form
+    "/submit-form": submit_form,
+    "/protocols": serve_protocols,
+    "/show_protocol": serve_protocol
 }
 
 def application(environ, start_response):
-
     if not "PATH_INFO" in environ:
-        return redirect_home(environ, start_response)
-
-    if environ['PATH_INFO'] in handlers: 
-        return handlers[environ['PATH_INFO']](environ, start_response)
-
+        return redirect_with_trilling_slash(environ, start_response)
+    handler = environ['PATH_INFO']
+    if handler.count("/") > 1:
+        handler = handler[0: handler.find("/", 1)]
+    if handler in handlers: 
+        return handlers[handler](environ, start_response)
     if '/resources/' in environ['PATH_INFO']:
         return serve_resource(environ, start_response)
-
     return not_supported(environ, start_response)
+
+def store_protocol(submitted): 
+    dd_mm_yy = time.strftime("%d.%m.%y", time.gmtime(time.time()))
+    f_name = "%s.%s.protocol" % (dd_mm_yy, submitted["changeset"])
+    path = os.path.join(APP_ROOT, 'storage', f_name)
+    f = open(path, 'w')
+    for key in get_ids():
+        f.write("%s: %s\n" % (key, submitted[key]))
+    f.close()
+    return path
+
+def check_test_index():
+    ID = 1
+    LABEL = 2
+    DESC = 3
+    ERROR = 4
+    state = DESC
+    in_file = open(TESTS, 'r')
+    # Any entry with a label: must be followed by an id: and a desc:
+    for line in in_file.readlines():
+        if line.startswith('id:'):
+            if not state == DESC:
+                state = ERROR
+                break
+            state = ID
+        elif line.startswith('label:'):
+            if not state == ID:
+                state = ERROR
+                break
+            state = LABEL
+        elif line.startswith('desc:'):
+            if not state == LABEL:
+                state = ERROR
+                break
+            state = DESC
+    in_file.close()
+    return not state == ERROR
+
+
+def add_ids_test_index():
+    import shutil
+    import tempfile
+    ID = 1
+    LABEL = 2
+    DESC = 3
+    ERROR = 4
+    state = DESC
+    in_file = open(TESTS, 'r')
+    f_id_count = open(ID_COUNT, 'r')
+    id_count = int(f_id_count.readline().strip())
+    f_id_count.close()
+    f_ids = open(IDS, 'a')
+    tmpfd, tmppath = tempfile.mkstemp(".tmp", "dftests.")
+    tmpfile = os.fdopen(tmpfd, "w")
+    for line in in_file.readlines():
+        if line.startswith('id:'):
+            if not state == DESC:
+                state = ERROR
+                break
+            state = ID
+        elif line.startswith('label:'):
+            if state == DESC:
+                tmpfile.write("id: %s\n" % id_count)
+                f_ids.write("id-%s\n" % id_count)
+                id_count += 1
+                state = ID
+            if not state == ID:
+                state = ERROR
+                break
+            state = LABEL
+
+        elif line.startswith('desc:'):
+            if not state == LABEL:
+                state = ERROR
+                break
+            state = DESC
+        tmpfile.write(line)
+    tmpfile.close()
+    in_file.close()
+    f_ids.close()
+    if state == ERROR:
+        raise AssertionError("Not well formed entry!")
+    shutil.copy(tmppath, TESTS)
+    os.unlink(tmppath)
+    f_id_count = open(ID_COUNT, 'w')
+    f_id_count.write(str(id_count))
+    f_id_count.close()
         
 if __name__ == '__main__':
     print "".join(serve_test_form(None, lambda status, response_headers: 0))
--- manual-test-suite/resources/markup.py
+++ manual-test-suite/resources/markup.py
@@ -1,4 +1,4 @@
-INDEX = """
+INDEX = """\
 <!doctype html>
 <h1>Opera Dragonfly manual test suite</h1>
 <ul>
@@ -7,21 +7,59 @@ INDEX = """
 </ul>
 """
 
-TEST_FORM = """
+PROTOCOL_LIST = """\
 <!doctype html>
-<script src="./resources/script.js"></script>
-<link rel="stylesheet" href="./resources/style.css">
-<h1>Integration Protocol</h1>
-<form method="POST" action="./submit-form">
-<table>
-<tr><td><td>tester: <td colspan="2" class="input"><input></tr>
-<tr><td><td>ichangeset: <td colspan="2" class="input"><input></tr>
+<h1>Opera Dragonfly test protocols</h1>
+<ul>
+    %s
+</ul>
 """
+
+LIST_LINK = """<li><a href="%s">%s</a>"""
+
+TEST_FORM = """\
+<!doctype html>
+<script src="%s/resources/script.js"></script>
+<link rel="stylesheet" href="%s/resources/style.css">
+<h1>Integration Protocol</h1>
+%s
+%s
+<table>
+"""
+
+FORM = """<form method="POST" action="./submit-form">"""
+
+
+LEGEND_MISSING = """
+<p class="legend-missing"> missing entries
+"""
+
+TR_TESTER_AND_CHANGESET_FORM = """
+<tr><td><td>tester: <td colspan="2" class="input"><input name="tester"></tr>
+<tr><td><td>changeset: <td colspan="2" class="input"><input name="changeset"></tr>
+"""
+
+TR_TESTER_AND_CHANGESET = """
+<tr><td><td>tester: <td colspan="2">%s</tr>
+<tr><td><td>changeset: <td colspan="2">%s</tr>
+"""
+
+TR_VALUE_CHECK = """
+<tr class="%s"><td><td>%s: <td colspan="2" class="input"><input name="%s"%s>%s</tr>
+"""
+
+VALUE_DISABLED = """ value="%s" disabled"""
+
+INPUT_HIDDEN = """<input type="hidden" name="%s" value="%s">"""
 
 TEST_FORM_END = """
 <tr><td colspan='4' class='submit'><input type='submit'></tr>
 </table>
 </form>
+"""
+
+TEST_END = """
+</table>
 """
 
 TR_TD_COLS_4 = """
@@ -44,5 +82,23 @@ TR_TEST = """
   <td>%s</td>
   <td><label><input type="radio" value="PASS" name="%s"> PASS</label></td>
   <td><label><input type="radio" value="FAIL" name="%s"> FAIL</label></td>
+</tr>
+"""
+
+TR_TEST_CHECK = """
+<tr class="%s" data-desc="%s">
+  <td class="item closed"><span></span></td>
+  <td>%s</td>
+  <td><label><input type="radio" value="PASS" name="%s"%s> PASS</label></td>
+  <td><label><input type="radio" value="FAIL" name="%s"%s> FAIL</label>%s</td>
+</tr>
+"""
+
+TR_TEST_PROTOCOL = """
+<tr data-desc="%s" class="%s">
+  <td class="item closed"><span></span></td>
+  <td>%s</td>
+  <td></td>
+  <td>%s</td>
 </tr>
 """
