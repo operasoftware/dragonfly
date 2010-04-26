436751bb97217b0a35354cc3e9c053b54992e43b
Chris K <chrisk@opera.com>
2010-04-26 18:42:01 +0200

    renamed parse to format and added a new parse method. use enums for token types. add new lines to the buffer. added a test case for the new parse method

--- src/ecma-debugger/formatter.js
+++ src/ecma-debugger/formatter.js
@@ -2,9 +2,57 @@
 
 window.cls.SimpleJSParser = function()
 {
-  /* adjusted to return fotmatted HTML */
+  /**
+    * This is a simple js parser. There are edge cases where it will fail,
+    * but with 'normal' js syntax it should work ok.
+    */
 
-  const DEFAULT = 0, SINGLE_QUOTE = 1, DOUBLE_QUOTE = 2, REG_EXP = 3, COMMENT = 4;
+  /* interface */
+
+  /**
+    * Creates html markup to syntax highlight a slice of a js scource file.
+    * @param {Object} script 
+    *     An object with the properties source_data, line_arr, state_arr.
+    *     source_data is the whole source file.
+    *     line_arr is a list with offset for each new line
+    *     satte_arr is the according list with the state for each line
+    * @param {Number} line The line number to start the formatting.
+    * @param {Number} max_line The count of maxium lines to create.
+    * @param {Number} state The start state.
+    */
+  this.format = function(script, line, max_line, state){};
+
+  /**
+    * Tokenize a give script string.
+    * @param {String} script_source The script string.
+    * @param {Array} token_arr The list of tokens.
+    * @param {Array} type_arr The list of token types.
+    * token_arr and type_arr must be passed as empty arrays. 
+    * This is a workaround for a missing standard destruction in js, 
+    * e.g. something like: tokens, token_types = parser.parse(script_source);
+    */
+  this.parse = function(script_source, token_arr, type_arr){};
+
+  /* privat */
+
+  /* optimized to return fotmatted HTML */
+
+  const 
+  DEFAULT_STATE = 0, 
+  SINGLE_QUOTE_STATE = 1, 
+  DOUBLE_QUOTE_STATE = 2, 
+  REG_EXP_STATE = 3, 
+  COMMENT_STATE = 4,
+  WHITESPACE = 1,
+  LINETERMINATOR = 2,
+  IDENTIFIER = 3,
+  NUMBER = 4,
+  STRING = 5,
+  PUNCTUATOR = 6,
+  IDENTIFIER = 7,
+  DIV_PUNCTUIATOR = 8,
+  REG_EXP = 9,
+  COMMENT = 10;
 
   var parser=null;
   var __source=null;
@@ -25,7 +73,31 @@ window.cls.SimpleJSParser = function()
   var __parse_error_line_buffer ='';
   var __parse_error_first_token = true;
   var __parse_error_description = "";
-  var WHITESPACE=
+
+  var __token_arr = null;
+  var __token_type_arr = null;
+  var __read_buffer_with_arrs = function()
+  {
+    if (__buffer)
+    {
+      __token_arr.push(__buffer);
+      __token_type_arr.push(__type)
+      if (__type==IDENTIFIER)
+      {
+        __previous_type=__type;
+        __previous_value = __buffer;
+      }
+    }
+    __buffer = '';
+  };
+  var __online_with_arrs = function()
+  {
+    __token_arr.push(__buffer);
+    __token_type_arr.push(LINETERMINATOR);
+    __buffer = '';
+  }
+
+  var WHITESPACE_CHARS =
   {
     '\u0009': 1, //  Tab <TAB>
     '\u000B': 1, //  Vertical Tab <VT>
@@ -33,7 +105,7 @@ window.cls.SimpleJSParser = function()
     '\u0020': 1, //  Space <SP>
     '\u00A0': 1  //  No-break space <NBSP>
   }
-  var LINETERMINATOR=
+  var LINETERMINATOR_CHARS =
   {
     '\u000A': 1, //  Line Feed <LF>
     '\u000D': 1, //  Carriage Return <CR>
@@ -41,7 +113,7 @@ window.cls.SimpleJSParser = function()
     '\u2028': 1, //  Line separator <LS>
     '\u2029': 1 //  Paragraph separator <PS>
   }
-  var NUMBER=
+  var NUMBER_CHARS =
   {
     '0': 1,
     '1': 1,
@@ -54,7 +126,7 @@ window.cls.SimpleJSParser = function()
     '8': 1,
     '9': 1
   }
-  var PUNCTUATOR=
+  var PUNCTUATOR_CHARS =
   {
     '{': '{',
     '}': '}',
@@ -80,7 +152,7 @@ window.cls.SimpleJSParser = function()
     ':': ':',
     '.': '.'
   }
-  var PUNCTUATOR_2=
+  var PUNCTUATOR_2_CHARS =
   {
     '=': '=',
     '+': '+',
@@ -90,12 +162,12 @@ window.cls.SimpleJSParser = function()
     '&': '&',
     '|': '|'
   }
-  var STRING_DELIMITER=
+  var STRING_DELIMITER_CHARS =
   {
     '"': 1,
     '\'': 1
   }
-  var HEX_NUMBER=
+  var HEX_NUMBER_CHARS =
   {
     '0': 1,
     '1': 1,
@@ -120,13 +192,13 @@ window.cls.SimpleJSParser = function()
     'E': 1,
     'F': 1
   }
-  var REG_EXP_FLAG=
+  var REG_EXP_FLAG_CHARS =
   {
     'g': 1,
     'i': 1,
     'm': 1
   }
-  var PUNCTUATOR_DIV_PREDECESSOR=
+  var PUNCTUATOR_DIV_PREDECESSOR_CHARS =
   {
     ')': 1,
     ']': 1,
@@ -143,32 +215,33 @@ window.cls.SimpleJSParser = function()
   }
   var default_parser=function(c)
   {
-    var CRLF='';
+    var CRLF = '';
     __previous_value='';
     while(c)
     {
-      if(c in WHITESPACE)
+      if(c in WHITESPACE_CHARS)
       {
         read_buffer();
-        __type='WHITESPACE';
+        __type=WHITESPACE;
         do
         {
           __buffer+=c;
           c=__source.charAt(++__pointer);
         }
-        while (c in WHITESPACE);
+        while (c in WHITESPACE_CHARS);
         read_buffer();
-        __type='IDENTIFIER';
+        __type=IDENTIFIER;
       }
 
-      if(c in LINETERMINATOR)
+      if(c in LINETERMINATOR_CHARS)
       {
         read_buffer();
-        CRLF=c;
-        CRLF+=c=__source.charAt(++__pointer);
-        if(CRLF in LINETERMINATOR)
+        __buffer = CRLF = c;
+        CRLF += c =__source.charAt(++__pointer);
+        if (CRLF in LINETERMINATOR_CHARS)
         {
-          c=__source.charAt(++__pointer);
+          __buffer = CRLF;
+          c = __source.charAt(++__pointer);
         }
         if(__online && __online())
         {
@@ -177,11 +250,11 @@ window.cls.SimpleJSParser = function()
         continue;
       }
     
-      if(c in NUMBER)
+      if(c in NUMBER_CHARS)
       {
         read_buffer();
         __buffer+=c;
-        __type='NUMBER';
+        __type=NUMBER;
         c=__source.charAt(++__pointer);
         if(c=='x' || c=='X') 
         {
@@ -195,12 +268,12 @@ window.cls.SimpleJSParser = function()
         continue;
       }
 
-      if(c in STRING_DELIMITER)
+      if(c in STRING_DELIMITER_CHARS)
       {
         read_buffer();
         __string_delimiter=c;
         __buffer+=c;
-        __type='STRING';
+        __type=STRING;
         if( string_parser(__source.charAt(++__pointer)) )
         {
           return __ret;
@@ -214,35 +287,35 @@ window.cls.SimpleJSParser = function()
         read_buffer();
         __buffer+=c;
         c=__source.charAt(++__pointer);
-        if(c in NUMBER)
+        if(c in NUMBER_CHARS)
         {
-          __type='NUMBER';
+          __type=NUMBER;
           c=number_dec_parser(c, 0);
           continue;
         }
         else
         {
-          __type='PUNCTUATOR';
+          __type=PUNCTUATOR;
           read_buffer();
-          __type='IDENTIFIER';
+          __type=IDENTIFIER;
           continue;
         }
       }
       
-      if(c in PUNCTUATOR)
+      if(c in PUNCTUATOR_CHARS)
       {
         read_buffer();
-        __type='PUNCTUATOR';
+        __type=PUNCTUATOR;
         do
         {
-          __buffer+=PUNCTUATOR[c];
+          __buffer+=PUNCTUATOR_CHARS[c];
           c=__source.charAt(++__pointer);
         }
-        while (c in PUNCTUATOR_2);
+        while (c in PUNCTUATOR_2_CHARS);
         __previous_value=__buffer;
         read_buffer();
         __previous_type=__type;
-        __type='IDENTIFIER';
+        __type=IDENTIFIER;
         continue;
       }
 
@@ -254,7 +327,7 @@ window.cls.SimpleJSParser = function()
         if(c=='*')  
         {
           __buffer+=c;
-          __type='COMMENT';
+          __type=COMMENT;
           if( multiline_comment_parser(__source.charAt(++__pointer)) )
           {
             return __ret;
@@ -266,7 +339,7 @@ window.cls.SimpleJSParser = function()
         if(c=='/')  
         {
           __buffer+=c;
-          __type='COMMENT';
+          __type=COMMENT;
           if( singleline_comment_parser(__source.charAt(++__pointer)) )
           {
             return __ret;
@@ -274,11 +347,11 @@ window.cls.SimpleJSParser = function()
           c = __source.charAt(__pointer);
           continue;
         }
-        if ((__previous_type=='IDENTIFIER' && !(__previous_value in REG_EXP_PREDECESSOR)) || 
-              __previous_type=='NUMBER' || 
-              (__previous_type=='PUNCTUATOR' && __previous_value in PUNCTUATOR_DIV_PREDECESSOR))
+        if ((__previous_type==IDENTIFIER && !(__previous_value in REG_EXP_PREDECESSOR)) || 
+              __previous_type==NUMBER || 
+              (__previous_type==PUNCTUATOR && __previous_value in PUNCTUATOR_DIV_PREDECESSOR_CHARS))
         {
-          __type='DIV_PUNCTUIATOR';
+          __type=DIV_PUNCTUIATOR;
           if(c=='=') 
           {
             __buffer+=c;
@@ -289,10 +362,10 @@ window.cls.SimpleJSParser = function()
           {
             read_buffer();
           }
-          __type='IDENTIFIER';
+          __type=IDENTIFIER;
           continue;
         }
-        __type='REG_EXP';
+        __type=REG_EXP;
         c=reg_exp_parser(c);
         continue;
       }
@@ -303,11 +376,11 @@ window.cls.SimpleJSParser = function()
         __buffer+=c in ESCAPE ? ESCAPE[c] : c;
         c=__source.charAt(++__pointer);
         if (!c 
-            || c in PUNCTUATOR  
+            || c in PUNCTUATOR_CHARS  
             || c=='/' 
-            || c in LINETERMINATOR  
-            || c in WHITESPACE 
-            || c in STRING_DELIMITER)
+            || c in LINETERMINATOR_CHARS  
+            || c in WHITESPACE_CHARS 
+            || c in STRING_DELIMITER_CHARS)
         {
           break;
         }
@@ -318,22 +391,22 @@ window.cls.SimpleJSParser = function()
 
   var number_hex_parser=function(c)
   {
-    while(c in HEX_NUMBER)
+    while(c in HEX_NUMBER_CHARS)
     {
       __buffer+=c;
       c=__source.charAt(++__pointer);
     }
     read_buffer();
     __previous_type=__type;
-    __type='IDENTIFIER';
+    __type=IDENTIFIER;
     return c;
   }
 
   var number_dec_parser=function(c, dot)
   {
-    while(c in NUMBER || c==dot)
+    while(c in NUMBER_CHARS || c === dot)
     {
-      if(c==dot)
+      if (c === dot)
       {
         dot = 0;
       }
@@ -349,15 +422,15 @@ window.cls.SimpleJSParser = function()
         __buffer+=c;
         c=__source.charAt(++__pointer);
       }
-      while(c in NUMBER)
+      while(c in NUMBER_CHARS)
       {
         __buffer+=c;
         c=__source.charAt(++__pointer);
       }
     }
     read_buffer();
-    __previous_type='NUMBER';
-    __type='IDENTIFIER';
+    __previous_type=NUMBER;
+    __type=IDENTIFIER;
     return c;
   }
 
@@ -370,12 +443,12 @@ window.cls.SimpleJSParser = function()
       {
         __buffer+=c;
         c=__source.charAt(++__pointer);
-        if(c in LINETERMINATOR)
+        if(c in LINETERMINATOR_CHARS)
         {
           read_buffer();
           CRLF=c;
           CRLF+=c=__source.charAt(++__pointer);
-          if(CRLF in LINETERMINATOR)
+          if(CRLF in LINETERMINATOR_CHARS)
           {
             c=__source.charAt(++__pointer);
           }
@@ -394,12 +467,12 @@ window.cls.SimpleJSParser = function()
       }
       if(c==__string_delimiter || 
         /* abort string parsing on a new line */
-        c in LINETERMINATOR) 
+        c in LINETERMINATOR_CHARS) 
       {
         __buffer+=c;
         read_buffer();
-        __previous_type='STRING';
-        __type='IDENTIFIER';
+        __previous_type=STRING;
+        __type=IDENTIFIER;
         break;
       }
       __buffer+=c in ESCAPE ? ESCAPE[c] : c;
@@ -412,12 +485,12 @@ window.cls.SimpleJSParser = function()
     var CRLF='';
     while(c)
     {
-      if(c in LINETERMINATOR)
+      if(c in LINETERMINATOR_CHARS)
       {
         read_buffer();
         CRLF=c;
         CRLF+=c=__source.charAt(++__pointer);
-        if(CRLF in LINETERMINATOR)
+        if(CRLF in LINETERMINATOR_CHARS)
         {
           c=__source.charAt(++__pointer);
         }
@@ -436,7 +509,7 @@ window.cls.SimpleJSParser = function()
           __buffer+=c;
           read_buffer();
           // don't change the previous type
-          __type='IDENTIFIER';
+          __type=IDENTIFIER;
           break;
         }
         continue;
@@ -451,14 +524,14 @@ window.cls.SimpleJSParser = function()
     var CRLF='';
     while(c)
     {
-      if(c in LINETERMINATOR) 
+      if(c in LINETERMINATOR_CHARS) 
       {
         read_buffer();
         // don't change the previous type
-        __type = 'IDENTIFIER';
+        __type = IDENTIFIER;
         CRLF = c;
         CRLF += c = __source.charAt(++__pointer);
-        if( CRLF in LINETERMINATOR )
+        if( CRLF in LINETERMINATOR_CHARS )
         {
           __pointer++;
         }
@@ -499,22 +572,22 @@ window.cls.SimpleJSParser = function()
       {
         __buffer+=c;
         c=__source.charAt(++__pointer);
-        while(c in REG_EXP_FLAG)
+        while(c in REG_EXP_FLAG_CHARS)
         {
           __buffer+=c;
           c=__source.charAt(++__pointer);
         }
         read_buffer();
-        __previous_type='REG_EXP';
-        __type='IDENTIFIER';
+        __previous_type=REG_EXP;
+        __type=IDENTIFIER;
         return c;
       }
       /* abort string parsing on a new line */
-      if(c in LINETERMINATOR) 
+      if(c in LINETERMINATOR_CHARS) 
       {
         read_buffer();
-        __previous_type='REG_EXP';
-        __type='IDENTIFIER';
+        __previous_type=REG_EXP;
+        __type=IDENTIFIER;
         return c;
       }
       __buffer+=c in ESCAPE ? ESCAPE[c] : c;
@@ -530,12 +603,12 @@ window.cls.SimpleJSParser = function()
     {
       switch (__type)
       {
-        case 'STRING':
+        case STRING:
         {
           __line += "<span class='string'>" +  __buffer + "</span>";
           break;
         }
-        case 'IDENTIFIER':
+        case IDENTIFIER:
         {
           if(__buffer in js_keywords)
           {
@@ -551,17 +624,17 @@ window.cls.SimpleJSParser = function()
           }
           break;
         }
-        case 'NUMBER':
+        case NUMBER:
         {
           __line += "<span class='number'>" +  __buffer + "</span>";
           break;
         }
-        case 'COMMENT':
+        case COMMENT:
         {
           __line += "<span class='comment'>" +  __buffer + "</span>";
           break;
         }
-        case 'REG_EXP':
+        case REG_EXP:
         {
           __line += "<span class='reg_exp'>" +  __buffer + "</span>";
           break;
@@ -571,7 +644,7 @@ window.cls.SimpleJSParser = function()
           __line += __buffer;
         }
       }
-      if(__type=='IDENTIFIER')
+      if(__type==IDENTIFIER)
       {
         __previous_type=__type;
         __previous_value = __buffer;
@@ -632,6 +705,7 @@ window.cls.SimpleJSParser = function()
     }
     __ret[__ret.length] = "<div>" + __line + "</div>";
     __line='';
+    __buffer = '';
     return (++__line_number) > __max_line_number;
   }
 
@@ -654,6 +728,7 @@ window.cls.SimpleJSParser = function()
       __ret[__ret.length] = "<div class='error-line error'>" + __line + "</div>";
     }
     __line='';
+    __buffer = '';
     return (++__line_number) > __max_line_number;
   }
 
@@ -672,9 +747,9 @@ window.cls.SimpleJSParser = function()
   }
 
   var states = [];
-  states[COMMENT] = function()
+  states[COMMENT_STATE] = function()
   {
-     __type = 'COMMENT';
+     __type = COMMENT;
     if( multiline_comment_parser(__source.charAt(__pointer)) )
     {
       return __ret;
@@ -682,17 +757,17 @@ window.cls.SimpleJSParser = function()
     __pointer++;
   };
 
-  // const DEFAULT = 0, SINGLE_QUOTE = 1, DOUBLE_QUOTE = 2, REG_EXP = 3, COMMENT = 4;
-  states[SINGLE_QUOTE] = function()
+  // const DEFAULT_STATE = 0, SINGLE_QUOTE_STATE = 1, DOUBLE_QUOTE_STATE = 2, REG_EXP_STATE = 3, COMMENT_STATE = 4;
+  states[SINGLE_QUOTE_STATE] = function()
   {
     opera.postError(ui_strings.DRAGONFLY_INFO_MESSAGE + 
       'state parsing not implemented in formatter.js for SINGLE_QUOTE');
   };
 
-  states[SINGLE_QUOTE] = function()
+  states[SINGLE_QUOTE_STATE] = function()
   {
     __string_delimiter= "'";
-    __type='STRING';
+    __type=STRING;
     if( string_parser(__source.charAt(__pointer)) )
     {
       return __ret;
@@ -700,11 +775,11 @@ window.cls.SimpleJSParser = function()
     __pointer++;
   };
 
-  states[DOUBLE_QUOTE] = function()
+  states[DOUBLE_QUOTE_STATE] = function()
   {
 
     __string_delimiter= '"';
-    __type='STRING';
+    __type=STRING;
     if( string_parser(__source.charAt(__pointer)) )
     {
       return __ret;
@@ -712,19 +787,19 @@ window.cls.SimpleJSParser = function()
     __pointer++;
   };
 
-  states[REG_EXP] = function()
+  states[REG_EXP_STATE] = function()
   {
     opera.postError(ui_strings.DRAGONFLY_INFO_MESSAGE + 
       'state parsing not implemented in formatter.js for REG_EXP');
   };
 
-  this.parse=function(script, line, max_line, state)
+  this.format = function(script, line, max_line, state)
   {
     __reset(line, max_line);
 
     parser=default_parser;
     __previous_type='';
-    __type='IDENTIFIER';
+    __type=IDENTIFIER;
     __source = script.source;
     var length=__source.length;
     __pointer = script.line_arr[line];
@@ -762,4 +837,26 @@ window.cls.SimpleJSParser = function()
       return __onfinish();
     }
   }
+
+  this.parse = function(script, token_arr, type_arr)
+  {
+
+    parser = default_parser;
+    __previous_type = '';
+    __type = IDENTIFIER;
+    __source = script;
+    __pointer = 0;
+    __token_arr = token_arr;
+    __token_type_arr = type_arr;
+
+    __online = function()
+    {
+      token_arr.push('\n');
+      type_arr.push(LINETERMINATOR);
+    }
+
+    read_buffer = __read_buffer_with_arrs;
+    parser(__source.charAt(__pointer));
+  } 
+  
 }
