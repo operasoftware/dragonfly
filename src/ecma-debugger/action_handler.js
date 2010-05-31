b8832525eec0cabfe8c7549947a09943ad99dfb9
Chris K <chrisk@opera.com>
2010-05-31 18:22:31 +0200

    removed handlers for ecma 6.0

--- src/ecma-debugger/action_handler.js
+++ src/ecma-debugger/action_handler.js
@@ -94,59 +94,6 @@ window.eventHandlers.click['examine-object-2'] = function(event, target)
   }
 };
 
-window.eventHandlers.click['examine-object'] = function(event, target)
-{
-  var
-  parent = target.parentNode,
-  parent_parent = parent.parentNode,
-  obj_id = parseInt(parent.getAttribute('obj-id')),
-  rt_id = parseInt(parent_parent.getAttribute('rt-id')),
-  data_id = parent_parent.getAttribute('data-id'),
-  data = null,
-  examine_object = parent.getElementsByTagName('examine-objects')[0],
-  cur = parent,
-  path = [];
-
-  while (cur && cur.parentNode && cur.parentNode.nodeName.toLowerCase() == 'examine-objects')
-  {
-    path.push(parseInt(cur.getAttribute('obj-id')));
-    cur = cur.parentNode.parentNode;
-  }
-  path.push(parseInt(parent_parent.getAttribute('obj-id')));
-  path.reverse();
-
-  if (window[data_id])
-  {
-    if (examine_object) // is unfolded
-    {
-      if (!target.disabled)
-      {
-        window[data_id].clearData(rt_id, obj_id, path);
-        parent.removeChild(examine_object);
-        target.style.removeProperty("background-position");
-      }
-    }
-    else
-    {
-      if (data = window[data_id].getData(rt_id, obj_id, path, arguments))
-      {
-        if (data.length)
-        {
-          examine_object = parent_parent.cloneNode(false);
-          examine_object.innerHTML = window[data_id].prettyPrint(data, path,
-            settings['inspection'].get("hide-default-properties"), window[data_id].filter_type);
-          parent.appendChild(examine_object);
-          target.style.backgroundPosition = "0px -11px";
-        }
-        else
-        {
-          target.disabled = true;
-        }
-      }
-    }
-  }
-};
-
 window.eventHandlers.click['show-scripts'] = function(event)
 {
   var runtime_id = event.target.getAttribute('runtime_id');
