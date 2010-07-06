4610e0c7ff7f64a9bc962e69280faf14f7dab295
Chris K <chrisk@opera.com>
2010-07-06 18:17:51 +0200

    Moved base inspection view in a own file.

--- src/ecma-debugger/objectinspection.6.0/nodeview.js
+++ src/ecma-debugger/objectinspection.6.0/nodeview.js
@@ -7,24 +7,6 @@ cls.EcmascriptDebugger["6.0"] || (cls.EcmascriptDebugger["6.0"] = {});
   * @extends ViewBase
   */
 
-cls.EcmascriptDebugger["6.0"].InspectionBaseView = function()
-{
-
-  this.createView = function(container)
-  {
-    var data_model = this._data || window.inspections[this._cur_data];
-    container.innerHTML = "";
-    if (data_model)
-      data_model.expand(this._create_view.bind(this, container, data_model));
-  };
-
-  this._create_view = function(container, data_model)
-  {
-    container.render(window.templates.inspected_js_object(data_model, false));
-  };
-
-}
-
 cls.EcmascriptDebugger["6.0"].DOMAttrsView = function(id, name, container_class)
 {
   this._data = null;
