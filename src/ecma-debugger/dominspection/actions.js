28087db6e3c93a7b09339d2fd2e2cecf5a0ae4fe
Chris K <chrisk@opera.com>
2010-07-28 16:36:00 +0200

    Moved all event handler code to actions.js.

--- src/ecma-debugger/dominspection/actions.js
+++ src/ecma-debugger/dominspection/actions.js
@@ -19,6 +19,110 @@ cls.DOMInspectorActions = function(id)
 
   const SCROLL_IN_PADDING = 30;
 
+  this.get_children = function (event)
+  {
+    var container = event.target.parentNode;
+    var level = parseInt(container.style.marginLeft) || 0;
+    var level_next = container.nextSibling && parseInt(container.nextSibling.style.marginLeft) || 0;
+    var ref_id = parseInt(container.getAttribute('ref-id'));
+    if (container = container.has_attr("parent-node-chain", "data-model-id"))
+    {
+      var model = window.dominspections[container.getAttribute('data-model-id')];
+      var target = document.getElementById('target-element');
+      var is_editable = container.hasAttribute('edit-handler');
+      var cb = null;
+      var target_id = 0;
+
+      if (container.contains(target))
+        target_id = parseInt(target.getAttribute('ref-id'));
+      if (level_next > level)
+      {
+        model.collapse(ref_id);
+        this._get_children_callback(container, model, target_id, is_editable);
+      }
+      else
+      {
+        cb = this._get_children_callback.bind(null, container, model, target_id, is_editable);
+        model.expand(cb, ref_id, event.ctrlKey ? 'subtree' : 'children');
+      }
+    }
+  }
+
+  this._get_children_callback = function(container, model, target_id, is_editable)
+  {
+    var tmpl = window.templates.inspected_dom_node(model, target_id, is_editable, true);
+    container.re_render(tmpl);
+  }
+
+  this.spotlight_node = function(event, current_target)
+  {
+    var 
+    obj_id = parseInt(current_target.getAttribute('ref-id')),
+    model_id = current_target.get_attr("parent-node-chain", "data-model-id"),
+    inspections = window.dominspections,
+    model = null,
+    target = null,
+    css_path = null;
+
+    if (model_id && obj_id)
+    {
+      model = inspections[model_id];
+      hostspotlighter.spotlight(obj_id,
+                                settings.dom.get('scroll-into-view-on-spotlight') && 
+                                obj_id != (inspections.active && inspections.active.target));
+      model.target = obj_id;
+      inspections.active = model;
+      window.messages.post("element-selected", {model: model, obj_id: obj_id, rt_id: model.getDataRuntimeId()});
+      if (current_target)
+      {
+        if (target = document.getElementById('target-element'))
+          target.removeAttribute('id');
+        if (!window.settings.dom.get('dom-tree-style') && /<\//.test(current_target.firstChild.textContent))
+        {
+          while ((current_target = current_target.previousSibling) && 
+                  current_target.getAttribute('ref-id') != obj_id);
+        }
+        topCell.statusbar.updateInfo(templates.breadcrumb(model, obj_id));
+      }
+      if (current_target)
+        current_target.id = 'target-element';
+    }
+  };
+
+  this.breadcrumb_link = function(event, target)
+  {
+    var 
+    obj_id = parseInt(target.getAttribute('obj-id')),
+    model_id = target.get_attr("parent-node-chain", "data-model-id"),
+    inspections = window.dominspections,
+    model = null;
+
+    if (model_id && obj_id)
+    {
+      model = inspections[model_id];
+      model.target = obj_id;
+      inspections.active = model;
+      window.messages.post("element-selected", {model: model, obj_id: obj_id, rt_id: model.getDataRuntimeId()});
+      var target = document.getElementById('target-element');
+      if (target)
+      {
+        target.removeAttribute('id');
+        while (target && !/container/i.test(target.nodeName) && (target = target.parentElement));
+        if (target)
+        {
+          var divs = target.getElementsByTagName('div'), div = null, i = 0;
+          for ( ; (div = divs[i]) && div.getAttribute('ref-id') != obj_id; i++);
+          if (div)
+          {
+            div.id = 'target-element';
+            window.helpers.scroll_dom_target_into_view();
+            hostspotlighter.spotlight(obj_id, true);
+          }
+        }
+      }
+    }
+  }
+
 
   var _is_script_node = function(target)
   {
@@ -658,149 +762,53 @@ cls.DOMInspectorEditKeyhandler = function(id)
 
 cls.DOMInspectorEditKeyhandler.prototype = BaseEditKeyhandler;
 
-
-
-window.eventHandlers.click['get-children'] = function (event)
+window.eventHandlers.click['get-children'] = function(event, target)
 {
-  var container = event.target.parentNode;
-  var level = parseInt(container.style.marginLeft) || 0;
-  var level_next = container.nextSibling && parseInt(container.nextSibling.style.marginLeft) || 0;
-  var ref_id = parseInt(container.getAttribute('ref-id'));
-  if (container = container.has_attr("parent-node-chain", "data-model-id"))
-  {
-    var model = window.dominspections[container.getAttribute('data-model-id')];
-    var target = document.getElementById('target-element');
-    var is_editable = container.hasAttribute('edit-handler');
-    var cb = null;
-    var target_id = 0;
+  window.actions['dom'].get_children(event, target);
+}
 
-    if (container.contains(target))
-      target_id = parseInt(target.getAttribute('ref-id'));
-    if (level_next > level)
-    {
-      model.collapse(ref_id);
-      window.eventHandlers.click['get-children'].callback(container, model, target_id, is_editable);
-    }
-    else
-    {
-      cb = window.eventHandlers.click['get-children'].callback.bind(null, container, model, target_id, is_editable);
-      model.expand(cb, ref_id, event.ctrlKey ? 'subtree' : 'children');
-    }
-  }
-};
-
-window.eventHandlers.click['get-children'].callback = function(container, model, target_id, is_editable)
+window.eventHandlers.click['spotlight-node'] = function(event, target)
 {
-  var tmpl = window.templates.inspected_dom_node(model, target_id, is_editable, true);
-  container.re_render(tmpl);
-};
-
-window.eventHandlers.click['spotlight-node'] = function(event, current_target)
-{
-  var 
-  obj_id = parseInt(current_target.getAttribute('ref-id')),
-  model_id = current_target.get_attr("parent-node-chain", "data-model-id"),
-  inspections = window.dominspections,
-  model = null,
-  target = null,
-  css_path = null;
-
-  if (model_id && obj_id)
-  {
-    model = inspections[model_id];
-    hostspotlighter.spotlight(obj_id,
-                              settings.dom.get('scroll-into-view-on-spotlight') && 
-                              obj_id != (inspections.active && inspections.active.target));
-    model.target = obj_id;
-    inspections.active = model;
-    window.messages.post("element-selected", {model: model, obj_id: obj_id, rt_id: model.getDataRuntimeId()});
-    if (current_target)
-    {
-      if (target = document.getElementById('target-element'))
-        target.removeAttribute('id');
-      if (!window.settings.dom.get('dom-tree-style') && /<\//.test(current_target.firstChild.textContent))
-      {
-        while ((current_target = current_target.previousSibling) && 
-                current_target.getAttribute('ref-id') != obj_id);
-      }
-      topCell.statusbar.updateInfo(templates.breadcrumb(model, obj_id));
-    }
-    if (current_target)
-      current_target.id = 'target-element';
-  }
-};
+  window.actions['dom'].spotlight_node(event, target);
+}
 
 window.eventHandlers.click['breadcrumb-link'] = function(event, target)
 {
-  var 
-  obj_id = parseInt(target.getAttribute('obj-id')),
-  model_id = target.get_attr("parent-node-chain", "data-model-id"),
-  inspections = window.dominspections,
-  model = null;
-
-  if (model_id && obj_id)
-  {
-    model = inspections[model_id];
-    model.target = obj_id;
-    inspections.active = model;
-    window.messages.post("element-selected", {model: model, obj_id: obj_id, rt_id: model.getDataRuntimeId()});
-    var target = document.getElementById('target-element');
-    if (target)
-    {
-      target.removeAttribute('id');
-      while (target && !/container/i.test(target.nodeName) && (target = target.parentElement));
-      if (target)
-      {
-        var divs = target.getElementsByTagName('div'), div = null, i = 0;
-        for ( ; (div = divs[i]) && div.getAttribute('ref-id') != obj_id; i++);
-        if (div)
-        {
-          div.id = 'target-element';
-          window.helpers.scroll_dom_target_into_view();
-          hostspotlighter.spotlight(obj_id, true);
-        }
-      }
-    }
-  }
-};
+  window.actions['dom'].breadcrumb_link(event, target);
+}
 
 window.eventHandlers.mouseover['spotlight-node'] = function(event, target)
 {
-  if(settings['dom'].get('highlight-on-hover'))
+  if(window.settings['dom'].get('highlight-on-hover'))
   {
-    hostspotlighter.soft_spotlight(parseInt(target.getAttribute('ref-id')));
+    window.hostspotlighter.soft_spotlight(parseInt(target.getAttribute('ref-id')));
   }
 }
 
 window.eventHandlers.click['dom-inspection-export'] = function(event)
 {
-  export_data.data = window.helpers.escapeTextHtml(views['dom'].export_markup());
-  topCell.showView('export_data');
+  window.export_data.data = window.helpers.escapeTextHtml(views['dom'].export_markup());
+  window.topCell.showView('export_data');
 };
 
-// button handlers
 window.eventHandlers.click['dom-inspection-snapshot'] = function(event, target)
 {
-  dom_data.get_snapshot();
+  window.dom_data.get_snapshot();
 };
 
 window.eventHandlers.click['df-show-live-source'] = function(event, target)
 {
-  debug_helpers.liveSource.open();
+  window.debug_helpers.liveSource.open();
 };
 
-window.eventHandlers.click['dom-resource-link'] = function(event, target)
+window.eventHandlers.click['dom-resource-link'] = function dom_resource_link (event, target)
 {
-  window.eventHandlers.dblclick['edit-dom'].delay(arguments.callee.execute, event, target);
+  window.eventHandlers.dblclick['edit-dom'].delay(dom_resource_link.execute, event, target);
 };
 
 window.eventHandlers.click['dom-resource-link'].execute = function(event, target)
 {
-  var
-  url = target.textContent,
-  rt_id = target.parentNode.parentNode.parentNode.getAttribute('rt-id')
-    // for the case of dom tree-style
-    || target.parentNode.parentNode.parentNode.parentNode.getAttribute('rt-id');
+  var url = target.textContent, rt_id = target.get_attr('parent-node-chain', 'rt-id');
   // TODO use the exec service to open new link when it's ready
   window.open(helpers.resolveURLS(runtimes.getURI(rt_id), url.slice(1, url.length - 1)), "_blank");
 };
@@ -811,11 +819,12 @@ window.eventHandlers.dblclick['edit-dom'] = (function(event, target)
   var handler = function(event, target)
   {
     click_timeouts.clear();
-    actions['dom'].editDOM(event, target);
+    window.actions['dom'].editDOM(event, target);
   };
-  handler.delay = function()
+  handler.delay = function(method)
   {
-    click_timeouts.set.apply(click_timeouts, [arguments[0], 300, arguments[1], arguments[2]]);
+    var args = [method, 300].concat(Array.prototype.slice.call(arguments).slice(1));
+    click_timeouts.set.apply(click_timeouts, args);
   }
   return handler;
 })();
