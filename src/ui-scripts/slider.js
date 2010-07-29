e59dd8b08f5a11499687f039bda928ba382273e4
Chris K <chrisk@opera.com>
2010-07-29 19:34:32 +0200

    Update the slider in the mouse event, but call the callback in the interval. Proper clean up in remove event.

--- src/ui-scripts/slider.js
+++ src/ui-scripts/slider.js
@@ -11,25 +11,15 @@
 
 Slider.prototype = new function()
 {
-  
+  const UPDATE_INTERVAL = 60;
   this._onmousemoveinterval = function(event)
   {
     if (this._is_active)
     {
-      if (this._event)
+      if (this.z != this._submitted_z)
       {
-        var z = 
-          (this._event.clientY - 
-          this._delta - 
-          this._ref_element.getBoundingClientRect().top) / 
-          this._pixel_range * this._range + 
-          this._min;
-        z = z > this._max ? this._max : z < this._min ? this._min : z;
-        if (z != this.z)
-        {
-          this.onz(this.z = z);
-        }
-      }
+        this.onz(this._submitted_z = this.z);
+      } 
     }
     else
     {
@@ -41,7 +31,17 @@ Slider.prototype = new function()
 
   this._onmousemove = function(event)
   {
-    this._event = event;
+    this._focus_catcher.focus();
+    var z = (event.clientY - 
+            this._delta - 
+            this._ref_element.getBoundingClientRect().top) / 
+            this._pixel_range * this._range + 
+            this._min;
+    z = z > this._max ? this._max : z < this._min ? this._min : z;
+    if (z != this.z)
+    {
+      this.z = z;
+    }
   }
   
   this._update_z = function(z)
@@ -57,7 +57,7 @@ Slider.prototype = new function()
       this._delta = event.clientY - target_box.top - (target_box.height / 2 >> 0);
       document.addEventListener('mousemove', this._onmousemove_bound, false);
       document.addEventListener('mouseup', this._onmouseup_bound, false);
-      this._interval = window.setInterval(this._onmousemoveinterval_bound, 30);
+      this._interval = window.setInterval(this._onmousemoveinterval_bound, UPDATE_INTERVAL);
       this._is_active = true;
     }
   }
@@ -69,17 +69,22 @@ Slider.prototype = new function()
     document.removeEventListener('mouseup', this._onmouseup_bound, false);
   }
   
-  this._onremove = function()
+  this._onremove = function(event)
   {
-    this._element.removeEventListener('mousedown', this._onmousedown_bound, false);
-    this._ref_element.removeEventListener('DOMnoderemoved', this._onremove_bound, false);
-    this._ref_element = null;
-    this._element = null;      
-    this._onmousedown_bound = null;
-    this._onmousemove_bound = null;
-    this._onmouseup_bound = null;
-    this._onremove_bound = null;
-    this._onmousemoveinterval_bound = null;
+    if (event.target.nodeType == 1 && event.target.contains(this._ref_element))
+    {
+      this._element.removeEventListener('mousedown', this._onmousedown_bound, false);
+      document.removeEventListener('DOMNoderemoved', this._onremove_bound, false);
+      this._focus_catcher.parentNode.removeChild(this._focus_catcher);
+      this._focus_catcher = null;
+      this._ref_element = null;
+      this._element = null;      
+      this._onmousedown_bound = null;
+      this._onmousemove_bound = null;
+      this._onmouseup_bound = null;
+      this._onremove_bound = null;
+      this._onmousemoveinterval_bound = null;
+    }
   }
   
   this._init = function(container, slider_base_class, slider_class, min, max)
@@ -87,21 +92,22 @@ Slider.prototype = new function()
     if (container instanceof Element)
     {
       container.render(window.templates.slider(slider_base_class, slider_class));
-      this._ref_element = document.getElementsByClassName(slider_base_class)[0];
-      this._element = document.getElementsByClassName(slider_class)[0];    
+      this._ref_element = container.getElementsByClassName(slider_base_class)[0];
+      this._element = container.getElementsByClassName(slider_class)[0]; 
+      this._focus_catcher = document.documentElement.render(window.templates.slider_focus_catcher());
       this._min = min;
       this._max = max;
       this._pixel_range = this._ref_element.getBoundingClientRect().height;
       this._range = max - min;
-      this._z = 0;    
+      this._z = 0; 
+      this._submitted_z = 0;
       this._onmousedown_bound = this._onmousedown.bind(this);
       this._onmousemove_bound = this._onmousemove.bind(this);
       this._onmouseup_bound = this._onmouseup.bind(this);
       this._onremove_bound = this._onremove.bind(this);
       this._onmousemoveinterval_bound = this._onmousemoveinterval.bind(this);
       this._element.addEventListener('mousedown', this._onmousedown_bound, false);
-      // TODO
-      //this._ref_element.addEventListener('DOMNodeRemoved', this._onremove_bound, false);
+      document.addEventListener('DOMNodeRemoved', this._onremove_bound, false);
     }
   }
   
