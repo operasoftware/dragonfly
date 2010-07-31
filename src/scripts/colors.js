67485dafc78b7f59dd5823c98de5e95ba958c584
Christian Krebs <chrisk@opera.com>
2010-07-31 20:55:52 +0200

    Finished the implemention of parseCSSColor.

--- src/scripts/colors.js
+++ src/scripts/colors.js
@@ -183,61 +183,154 @@ Colors.prototype = new function()
     }
     return this;
   }
+  
+  this._css_color_keywords =
+  {
+    black: '000000', silver: 'C0C0C0', gray: '808080', white: 'FFFFFF', 
+    maroon: '800000', red: 'FF0000', purple: '800080', fuchsia: 'FF00FF', 
+    green: '008000', lime: '00FF00', olive: '808000', yellow: 'FFFF00', 
+    navy: '000080', blue: '0000FF', teal: '008080', aqua: '00FFFF', 
+    aliceblue: 'f0f8ff', antiquewhite: 'faebd7', aqua: '00ffff', 
+    aquamarine: '7fffd4', azure: 'f0ffff', beige: 'f5f5dc', bisque: 'ffe4c4', 
+    black: '000000', blanchedalmond: 'ffebcd', blue: '0000ff', 
+    blueviolet: '8a2be2', brown: 'a52a2a', burlywood: 'deb887', 
+    cadetblue: '5f9ea0', chartreuse: '7fff00', chocolate: 'd2691e', 
+    coral: 'ff7f50', cornflowerblue: '6495ed', cornsilk: 'fff8dc', 
+    crimson: 'dc143c', cyan: '00ffff', darkblue: '00008b', darkcyan: '008b8b', 
+    darkgoldenrod: 'b8860b', darkgray: 'a9a9a9', darkgreen: '006400', 
+    darkgrey: 'a9a9a9', darkkhaki: 'bdb76b', darkmagenta: '8b008b', 
+    darkolivegreen: '556b2f', darkorange: 'ff8c00', darkorchid: '9932cc', 
+    darkred: '8b0000', darksalmon: 'e9967a', darkseagreen: '8fbc8f', 
+    darkslateblue: '483d8b', darkslategray: '2f4f4f', darkslategrey: '2f4f4f', 
+    darkturquoise: '00ced1', darkviolet: '9400d3', deeppink: 'ff1493', 
+    deepskyblue: '00bfff', dimgray: '696969', dimgrey: '696969', 
+    dodgerblue: '1e90ff', firebrick: 'b22222', floralwhite: 'fffaf0', 
+    forestgreen: '228b22', fuchsia: 'ff00ff', gainsboro: 'dcdcdc', 
+    ghostwhite: 'f8f8ff', gold: 'ffd700', goldenrod: 'daa520', gray: '808080', 
+    green: '008000', greenyellow: 'adff2f', grey: '808080', honeydew: 'f0fff0', 
+    hotpink: 'ff69b4', indianred: 'cd5c5c', indigo: '4b0082', ivory: 'fffff0', 
+    khaki: 'f0e68c', lavender: 'e6e6fa', lavenderblush: 'fff0f5', 
+    lawngreen: '7cfc00', lemonchiffon: 'fffacd', lightblue: 'add8e6', 
+    lightcoral: 'f08080', lightcyan: 'e0ffff', lightgoldenrodyellow: 'fafad2', 
+    lightgray: 'd3d3d3', lightgreen: '90ee90', lightgrey: 'd3d3d3', 
+    lightpink: 'ffb6c1', lightsalmon: 'ffa07a', lightseagreen: '20b2aa', 
+    lightskyblue: '87cefa', lightslategray: '778899', lightslategrey: '778899', 
+    lightsteelblue: 'b0c4de', lightyellow: 'ffffe0', lime: '00ff00', 
+    limegreen: '32cd32', linen: 'faf0e6', magenta: 'ff00ff', maroon: '800000', 
+    mediumaquamarine: '66cdaa', mediumblue: '0000cd', mediumorchid: 'ba55d3', 
+    mediumpurple: '9370db', mediumseagreen: '3cb371', mediumslateblue: '7b68ee', 
+    mediumspringgreen: '00fa9a', mediumturquoise: '48d1cc', 
+    mediumvioletred: 'c71585', midnightblue: '191970', mintcream: 'f5fffa', 
+    mistyrose: 'ffe4e1', moccasin: 'ffe4b5', navajowhite: 'ffdead', 
+    navy: '000080', oldlace: 'fdf5e6', olive: '808000', olivedrab: '6b8e23', 
+    orange: 'ffa500', orangered: 'ff4500', orchid: 'da70d6', 
+    palegoldenrod: 'eee8aa', palegreen: '98fb98', paleturquoise: 'afeeee', 
+    palevioletred: 'db7093', papayawhip: 'ffefd5', peachpuff: 'ffdab9', 
+    peru: 'cd853f', pink: 'ffc0cb', plum: 'dda0dd', powderblue: 'b0e0e6', 
+    purple: '800080', red: 'ff0000', rosybrown: 'bc8f8f', royalblue: '4169e1', 
+    saddlebrown: '8b4513', salmon: 'fa8072', sandybrown: 'f4a460', 
+    seagreen: '2e8b57', seashell: 'fff5ee', sienna: 'a0522d', silver: 'c0c0c0', 
+    skyblue: '87ceeb', slateblue: '6a5acd', slategray: '708090', 
+    slategrey: '708090', snow: 'fffafa', springgreen: '00ff7f', 
+    steelblue: '4682b4', tan: 'd2b48c', teal: '008080', thistle: 'd8bfd8', 
+    tomato: 'ff6347', turquoise: '40e0d0', violet: 'ee82ee', wheat: 'f5deb3', 
+    white: 'ffffff', whitesmoke: 'f5f5f5', yellow: 'ffff00', 
+    yellowgreen: '9acd32'
+  }
 
-  this._pc_3hex = function(c){return c + c};
-  this._pc_trim_int = function(c){return parseInt(c.trim())};
+  this.KEYWORD = 'keyword';
+  this.HEX = 'hex';
+  this.RGB = 'rgb';
+  this.RGBA = 'rgba';
+  this.HSL = 'hsl';
+  this.HSLA = 'hsla';
+  this._double_hex = function(c){return c + c};
+  this._trim_int = function(c){return parseInt(c.trim())};
+  this._re_hex6 = /^#[0-9a-fA-F]{6}$/;
+  this._re_hex3 = /^#[0-9a-fA-F]{3}$/;
+  // helper call to construct reg exps to verify css color tokens
+  (function()
+  {
+    var 
+    // rgb component
+    c = "\\s*(?:[0-1]?\\d{1,2}|2[0-4]\\d|25[0-5])\\s*",
+    // alpha
+    a = "\\s*0?\\.\\d+\\s*",
+    // optional float
+    of = "(?:\\.\\d+)?",
+    // hue
+    h = "\\s*(?:[0-2]?\\d{1,2}" + of + "|3[0-5]\\d" + of + "|360(?:\\.0+)?)\\s*",
+    // saturation and luminosity
+    sl = "\\s*(?:[0]?\\d{1,2}" + of + "|100(?:\\.0+)?)%\\s*";
 
+    this._re_rgb = new RegExp("^rgb\\(" + [c, c, c].join(',') + "\\)$"); 
+    this._re_rgba = new RegExp("^rgba\\(" + [c, c, c, a].join(',') + "\\)$"); 
+    this._re_hsl = new RegExp("^hsl\\(" + [h, sl, sl].join(',') + "\\)$");   
+    this._re_hsla = new RegExp("^hsla\\(" + [h, sl, sl, a].join(',') + "\\)$"); 
+  }).apply(this);
   this.parseCSSColor = function(in_str)
   {
-    var str = in_str.trim(), color = null, alpha = 0;
+    var str = in_str.trim(), color = null;
+    this.cssvalue = in_str;
+    this.alpha = null;
+    this.type = '';
     if (str == "transparent")
     {
       this.setHex("000000");
-      return {cssvalue: in_str, type: 'rgba', color: color, alpha: 0, hhex: this.hhex};
+      this.alpha = 0;
+      this.type = this.KEYWORD; 
+      return this;
     }
-    if (/^#[0-9a-fA-F]{6}$/.test(str))
+    if (in_str in this._css_color_keywords)
     {
-      color =  str.replace('#', '');
-      this.setHex(color);
-      return {cssvalue: in_str, type: 'hex', color: color, hhex: this.hhex};
+      this.setHex(this._css_color_keywords[in_str]);
+      this.type = this.KEYWORD;
+      return this;
     }
-    if (/^#[0-9a-fA-F]{3}$/.test(str))
+    if (this._re_hex6.test(str))
     {
-      color = Array.prototype.slice.call(str.replace('#', '')).map(this._pc_3hex).join('');
-      this.setHex(color);
-      return {cssvalue: in_str, type: 'hex', color: color, hhex: this.hhex};
+      this.setHex(str.slice(1));
+      this.type = this.HEX;
+      return this;
     }
-    if (/^rgb\(.*\)$/.test(str))
+    if (this._re_hex3.test(str))
     {
-      color = str.slice(4, str.length - 1).split(',').map(this._pc_trim_int);
-      this.setRGB(color);
-      return {cssvalue: in_str, type: 'rgb', color: color, hhex: this.hhex};
+      this.setHex(str.slice(1).split('').map(this._double_hex).join(''));
+      this.type = this.HEX;
+      return this;
     }
-    if (/^rgba\(.*\)$/.test(str))
+    if (this._re_rgb.test(str))
+    {
+      this.setRGB(str.slice(4, str.length - 1).split(',').map(this._trim_int));
+      this.type = this.RGB;
+      return this;
+    }
+    if (this._re_rgba.test(str))
     {
       color = str.slice(5, str.length - 1).split(',');
-      alpha = parseFloat(color.pop().trim());
-      color = color.map(this._pc_trim_int);
-      this.setRGB(color);
-      return {cssvalue: in_str, type: 'rgba', color: color, alpha: alpha, hhex: this.hhex};
+      this.alpha = parseFloat(color.pop().trim());
+      this.setRGB(color.map(this._trim_int));
+      this.type = this.RGBA;
+      return this;
     }
-    if (/^hsl\(\s*\d{1,3}\s*,\s*\d{1,3}%\s*,\s*\d{1,3}%\s*\)$/.test(str))
+    if (this._re_hsl.test(str))
     {
-      color = str.slice(4, str.length - 1).split(',').map(this._pc_trim_int);
+      color = str.slice(4, str.length - 1).split(',').map(this._trim_int);
       this.setHue(color[0]);
       this.setSaturation(color[1]);
       this.setLuminosity(color[2]);
-      return {cssvalue: in_str, type: 'hsl', color: color, hhex: this.hhex};
+      this.type = this.HSL;
+      return this;
     }
-    if (/^hsla\(\s*\d{1,3}\s*,\s*\d{1,3}%\s*,\s*\d{1,3}%\s*,\s*(?:0?\.\d+|1)\s*\)$/.test(str))
+    if (this._re_hsla.test(str))
     {
       color = str.slice(5, str.length - 1).split(',');
-      alpha = parseFloat(color.pop().trim());
-      color = [parseInt(color[0].trim()), parseFloat(color[1].trim()), parseFloat(color[2].trim())];
-      this.setHue(color[0]);
-      this.setSaturation(color[1]);
-      this.setLuminosity(color[2]);
-      return {cssvalue: in_str, type: 'hsla', color: color, alpha: alpha, hhex: this.hhex};
+      this.setHue(parseInt(color[0].trim()));
+      this.setSaturation(parseFloat(color[1].trim()));
+      this.setLuminosity(parseFloat(color[2].trim()));
+      this.alpha = parseFloat(color[3].trim());
+      this.type = this.HSLA;
+      return this
     }
     return null;
   }
@@ -366,6 +459,8 @@ Colors.prototype = new function()
       this.__hsv[1] = color.__hsv[1]; 
       this.__hsv[2] = color.__hsv[2]; 
       this.alpha = color.alpha;
+      this.type = color.type; // hex, rgb, rgba, hsl, hsla, keyword
+      this.cssvalue = clor.cssvalue;
     }
   }
 
@@ -403,6 +498,18 @@ Colors.prototype = new function()
   });
   this.__defineSetter__('hsl', function(val){}); // parse hsl?
   
+  this.__defineGetter__('hsla', function()
+  {
+    return (
+      "hsla(" + 
+      this.getHue() + ', ' +
+      this.getSaturation() + '%, ' +
+      this.getLuminosity() + '%, ' +
+      (typeof this.alpha == 'number' ? this.alpha.toFixed(3) : '1') +
+      ")");
+  });
+  this.__defineSetter__('hsla', function(val){}); // parse hsl?
+  
   this.__defineGetter__('hsv', function()
   {
     return (
@@ -424,6 +531,18 @@ Colors.prototype = new function()
       ")");
   });
   this.__defineSetter__('rgb', function(val){}); // parse rgb?
+  
+  this.__defineGetter__('rgba', function()
+  {
+    return (
+      "rgba(" + 
+      this.getRed() + ', ' +
+      this.getGreen() + ', ' +
+      this.getBlue() + ', ' +
+      (typeof this.alpha == 'number' ? this.alpha.toFixed(3) : '1') +
+      ")");
+  });
+  this.__defineSetter__('rgba', function(val){}); // parse rgb?
   
   this.__defineGetter__('hhex', function()
   {
