19af63cab5b30030eaacb9adb75da053939a8e63
Rune Halvorsen <runeh@opera.com>
2010-08-10 13:52:33 +0200

    Use dict style for declaring members of class in inspectableobjectview

--- src/repl/inspectableobjectview.js
+++ src/repl/inspectableobjectview.js
@@ -6,8 +6,12 @@ window.cls.InspectableObjectView = function(rt_id, obj_id, name, root)
   this.model = new cls.InspectableJSObject(rt_id, obj_id, name);
   this.show_root = root == undefined ? true : false;
   this.expanded = false;
+};
 
-  this.render = function()
+
+window.cls.InspectableObjectView.prototype =
+{
+  render: function()
   {
     var tpl = window.templates.inspected_js_object(this.model, this.show_root);
     if (!tpl)
@@ -15,31 +19,35 @@ window.cls.InspectableObjectView = function(rt_id, obj_id, name, root)
       opera.postError(ui_strings.DRAGONFLY_INFO_MESSAGE + " Inspectable object template yielded no data");
     }
     return tpl;
-  };
+  },
 
-  this.expand = function(cb)
+  expand: function(cb)
   {
     this.model.expand(function(){this.expanded = true; cb();}.bind(this));
-  };
+  }
 };
-
 
 
 window.cls.InspectableDomNodeView = function(rt_id, obj_id, name, root)
 {
+  this.obj_id = obj_id;
   this.model = new cls.InspectableDOMNode(rt_id, obj_id);
   this.show_root = root == undefined ? true : false;
   this.expanded = false;
+};
 
-  this.render = function()
+window.cls.InspectableDomNodeView.prototype =
+{
+
+  render: function()
   {
     var tpl = window.templates.inspected_dom_node(this.model, this.show_root);
     return tpl;
-  };
+  },
 
-  this.expand = function(cb)
+  expand: function(cb)
   {
+    var obj_id = this.obj_id;
     this.model.expand(function(){this.expanded = true; cb();}.bind(this), obj_id, "subtree");
-  };
-
+  }
 };
