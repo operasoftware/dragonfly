56dca7778dfecf39d87989bb12f510df15790708
Rune Halvorsen <runeh@opera.com>
2010-09-27 21:38:50 +0200

    Added document manager to app builder

--- src/build-application/build_application.js
+++ src/build-application/build_application.js
@@ -165,7 +165,8 @@ window.app.build_application = function(on_services_created, on_services_enabled
     'exec',
     'window-manager',
     'ecmascript-debugger',
-    'resource-manager'
+    'resource-manager',
+    'document-manager'
   ].forEach(create_raw_interface);
   var params = this.helpers.parse_url_arguments();
   if(params.debug)
