6e6156d8be7531dd0a620f39d5877551cdcfd9b6
Rune Halvorsen <runeh@opera.com>
2010-10-14 16:04:11 +0200

    Added a bunch of public methods for buffermanager and added tests

--- src/repl/buffermanager.js
+++ src/repl/buffermanager.js
@@ -7,7 +7,6 @@
 cls.BufferManager = function(textarea)
 {
   this._textarea = textarea;
-
   this.handle = function(evt) {
       return this._handle_event(evt);
   };
@@ -19,16 +18,16 @@ cls.BufferManager = function(textarea)
       switch (evt.keyCode)
       {
         case 97: // a key. ctrl-a == move to start of line
-          this._move_to_beginning_of_line();
+          this.move_to_beginning_of_line();
           return true;
         case 101: // e key. ctrl-e == move to end of line
-          this._move_to_end_of_line();
+          this.move_to_end_of_line();
           return true;
         case 107: // k key. ctrl-k == kill to end of line
-          this._kill_to_end_of_line();
+          this.kill_to_end_of_line();
           return true;
         case 119: // w key. ctrl-w == kill word backwards
-          this._kill_word_backwards();
+          this.kill_word_backwards();
           return true;
       }
     }
@@ -45,19 +44,23 @@ cls.BufferManager = function(textarea)
     this.put_cursor(this._textarea.value.length);
   };
 
-  this._kill_to_end_of_line = function()
+  this.kill_to_end_of_line = function()
   {
     var pos = this._textarea.selectionStart;
     this._textarea.value = this._textarea.value.slice(0, pos);
   };
 
-  this._kill_word_backwards = function()
+  this.kill_word_backwards = function()
   {
     var textarea = this._textarea;
-    var str_before_cursor = textarea.value.slice(0, textarea.selectionStart)
+    var str_before_cursor = textarea.value.slice(0, textarea.selectionStart);
     var char_before_cursor = str_before_cursor[str_before_cursor.length-1];
     var new_str_before_cursor = "";
     var replace_regexp = "";
+
+    /**
+     * Word boundries are whitespace and non-alphanumeric characters
+     */
 
     // Somewhat VIM inspired
     if (/\s/.test(char_before_cursor))
@@ -84,4 +87,39 @@ cls.BufferManager = function(textarea)
     this._textarea.selectionStart = offset;
     this._textarea.selectionEnd = offset;
   };
+
+  this.get_cursor = function()
+  {
+    return this._textarea.selectionStart;
+  };
+
+  this.set_value = function(value, cursorpos)
+  {
+    this._textarea.value = value;
+    if (cursorpos !== undefined)
+    {
+      this.put_cursor(cursorpos);
+    }
+  };
+
+  this.get_value = function(start, end)
+  {
+    if (start === undefined)
+    {
+      return this._textarea.value;
+    }
+    else if (end === undefined)
+    {
+      return this._textarea.value.slice(start);
+    }
+    else
+    {
+      return this._textarea.value.slice(start, end);
+    }
+  };
+
+  this.clear = function()
+  {
+    this.set_value("");
+  }
 }
