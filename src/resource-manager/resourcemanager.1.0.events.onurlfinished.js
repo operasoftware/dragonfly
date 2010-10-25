4939c11f009e2b8af35661861b18555ea5762a6b
Rune Halvorsen <runeh@opera.com>
2010-10-25 17:58:26 +0200

    Fixed missing fields in autogenerated class

--- src/resource-manager/resourcemanager.1.0.events.onurlfinished.js
+++ src/resource-manager/resourcemanager.1.0.events.onurlfinished.js
@@ -9,6 +9,8 @@ cls.ResourceManager["1.0"].UrlFinished = function(arr)
   // cls.ResourceManager["1.0"].UrlFinished.Result
   this.result = arr[1];
   this.time = arr[2];
+  this.mimeType = arr[3];
+  this.characterEncoding = arr[4];
 };
 cls.ResourceManager["1.0"].UrlFinished.Result =
 {
