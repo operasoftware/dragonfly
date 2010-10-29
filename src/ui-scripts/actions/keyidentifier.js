5850d4ae3c595f47074fa6fb880a59c70d70694f
Christian Krebs <chrisk@opera.com>
2010-10-29 20:32:18 +0200

    Syntax fix.

--- src/ui-scripts/actions/keyidentifier.js
+++ src/ui-scripts/actions/keyidentifier.js
@@ -4,7 +4,7 @@ var KeyIdentifier = function()
   
   this._key_name_map = 
   {
-    "TAB": 9, "ENTER": 13 "ESCAPE": 27, "SPACE": 32, "BACKSPACE": 8,
+    "TAB": 9, "ENTER": 13, "ESCAPE": 27, "SPACE": 32, "BACKSPACE": 8,
     "LEFT": 37, "UP": 38, "RIGHT": 39, "DOWN": 40, "DELETE": 46,
     "F1": 112, "F2": 113, "F3": 114, "F4": 115, "F5": 116, "F6": 117,
     "F7": 118, "F8": 119, "F9": 120, "F10": 121, "F11": 122, "F12": 123,
