889eaade244f93837e3e0aded5edc59bcf41e8cd
Rune Halvorsen <runeh@opera.com>
2010-11-24 19:16:44 +0100

    Use SortableTable class instead of custom table

--- src/resource-manager/resource_all_view.js
+++ src/resource-manager/resource_all_view.js
@@ -18,7 +18,8 @@ cls.ResourceManagerAllView = function(id, name, container_class, html, default_h
   this._render_main_view = function(container)
   {
     var ctx = this._service.get_request_context();
-    container.clearAndRender(templates.all_resources(ctx, this._columns, this._sort_by, this._reverse));
+    this._table = new SortableTable(this._tabledef, ctx.resources.slice(0))
+    container.clearAndRender(this._table.render())
   };
 
   this._handle_open_resource_bound = function(evt, target)
@@ -42,6 +43,46 @@ cls.ResourceManagerAllView = function(id, name, container_class, html, default_h
     }
     this.update();
   }.bind(this);
+
+  this._tabledef = {
+    columns: {
+      icon: {
+        label: "Icon",
+        renderer: function(res) {
+          return templates.resource_icon(res.urlfinished ?
+                                         res.urlfinished.mimeType :
+                                         null) },
+      },
+      host: {
+        label: "Host",
+        getter: function(res) { return cls.ResourceUtil.url_host(res.urlload.url) },
+      },
+      path: {
+        label: "Path",
+        getter: function(res) { return cls.ResourceUtil.url_path(res.urlload.url) },
+      },
+      mime: {
+        label: "Mime",
+        getter: function(res) { return res.urlfinished ? res.urlfinished.mimeType : "n/a" }
+      },
+      type: {
+        label: "Type",
+        getter: function(res) { return res.urlfinished ? cls.ResourceUtil.mime_to_type(res.urlfinished.mimeType) : "n/a" }
+      },
+      size: {
+        label: "Size",
+        getter: function(res) { return String(res.urlfinished ? res.urlfinished.contentLength : "n/a") },
+      },
+      size_h: {
+        label: "Size(h)",
+        getter: function(res) {
+          return String(res.urlfinished ?
+                        cls.ResourceUtil.bytes_to_human_readable(res.urlfinished.contentLength) :
+                        "n/a")
+        }
+      }
+    },
+  }
 
   var eh = window.eventHandlers;
   eh.click["resources-all-open"] = this._handle_open_resource_bound;
