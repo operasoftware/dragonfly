c7fd55f6e8e70abde2be1fd4898c72e2d6e4a999
dherzog <devnull@localhost>
2010-12-07 13:32:13 +0100

    removing clear-repl shortcut on mac until we can support ctrl-l there

--- src/scripts/ini.js
+++ src/scripts/ini.js
@@ -206,43 +206,43 @@
       {
         "default":
         {
-         "cmd l": "clear-repl",
+          // "ctrl l": "clear-repl",
         },
         "single-line-edit":
         {
-         "up": "backlog-prev",
-         "down": "backlog-next",
-         "tab": "autocomplete",
-         "enter": "eval",
-         "shift enter": "enter-multiline-mode",
-         // "ctrl l": "clear",
-         // "ctrl k": "kill-to-end-of-line",
-         // "ctrl u": "kill-to-beginning-of-line", // non-existent on mac afaict
-         // "ctrl e": "move-to-end-of-LINE",
-         // "ctrl a": "move-to-beginning-of-line",
-         // "ctrl w": "kill-word-backwards", // non-existent on mac afaict
-         // "ctrl y": "yank" // non-existent on mac afaict
+          "up": "backlog-prev",
+          "down": "backlog-next",
+          "tab": "autocomplete",
+          "enter": "eval",
+          "shift enter": "enter-multiline-mode",
+          // "ctrl l": "clear",
+          // "ctrl k": "kill-to-end-of-line",
+          // "ctrl u": "kill-to-beginning-of-line", // non-existent on mac afaict
+          // "ctrl e": "move-to-end-of-LINE",
+          // "ctrl a": "move-to-beginning-of-line",
+          // "ctrl w": "kill-word-backwards", // non-existent on mac afaict
+          // "ctrl y": "yank" // non-existent on mac afaict
         },
         "multi-line-edit":
         {
-         "shift enter": "exit-multiline-mode",
-         "cmd enter": "eval",
-         "tab": "insert-tab-at-point",
+          "shift enter": "exit-multiline-mode",
+          "cmd enter": "eval",
+          "tab": "insert-tab-at-point",
         },
         "autocomplete":
         {
-         "left": "prev-completion",
-         "right": "next-completion",
-         "tab": "next-completion",
-         "shift tab": "prev-completion",
-         "enter": "commit",
-         "[": "commit-and-insert",
-         "]": "commit-and-insert",
-         ".": "commit-and-insert",
-         "(": "commit-and-insert",
-         ")": "commit-and-insert",
-         "escape": "cancel-completion",
-         // "ctrl l": "cancel-input",
+          "left": "prev-completion",
+          "right": "next-completion",
+          "tab": "next-completion",
+          "shift tab": "prev-completion",
+          "enter": "commit",
+          "[": "commit-and-insert",
+          "]": "commit-and-insert",
+          ".": "commit-and-insert",
+          "(": "commit-and-insert",
+          ")": "commit-and-insert",
+          "escape": "cancel-completion",
+          // "ctrl l": "cancel-input",
         },
       },
     },
