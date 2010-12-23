1ba5fd2bd3c05d9fba9b78994ab7130e2b898e94
dstorey <dstorey@opera.com>
2010-12-23 20:23:57 +0000

    Added missing languages inc. British English, Bulgarian, Slovakian, Serbian, Romanian. Fixed Belorussian name

--- src/ecma-debugger/templates.js
+++ src/ecma-debugger/templates.js
@@ -1,4 +1,4 @@
-﻿window.templates = window.templates || {};
+window.templates = window.templates || {};
 
 (function()
 {
@@ -552,7 +552,12 @@ MODE ::= "<mode>"
       {
         browserLanguge: "be",
         key: "be",
-        name: "български език"
+        name: "Беларуская"
+      },
+      {
+        browserLanguge: "bg",
+        key: "bg",
+        name: "Български"
       },
       {
         browserLanguge: "cs",
@@ -577,7 +582,12 @@ MODE ::= "<mode>"
       {
         browserLanguge: "en",
         key: "en",
-        name: "English"
+        name: "U.S. English"
+      },
+      {
+        browserLanguge: "en-GB",
+        key: "en-GB",
+        name: "British English"
       },
       {
         browserLanguge: "es-ES",
@@ -695,9 +705,24 @@ MODE ::= "<mode>"
         name: "Português (Brasil)"
       },
       {
+        browserLanguge: "ro",
+        key: "ro",
+        name: "Română"
+      },
+      {
         browserLanguge: "ru",
         key: "ru",
         name: "Русский язык"
+      },
+      {
+        browserLanguge: "sk",
+        key: "sk",
+        name: "Slovenčina"
+      },
+      {
+        browserLanguge: "sr",
+        key: "sr",
+        name: "српски"
       },
       {
         browserLanguge: "sv",
