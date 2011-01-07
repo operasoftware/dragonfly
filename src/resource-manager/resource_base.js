7bc3469c23e49f120f8bdd35d9594374bd296884
Rune Halvorsen <runeh@opera.com>
2011-01-07 18:38:15 +0100

    Fixed more resource base class stuff

--- src/resource-manager/resource_base.js
+++ src/resource-manager/resource_base.js
@@ -1,84 +1,60 @@
 window.cls = window.cls || {};
 
 
+cls.ResourceDetailBase = function()
+{
+  this.resourcedata = null;
+  this.container = null;
+  this.drawer = null;
+
+  this.createView = function(container)
+  {
+    container.clearAndRender(this.drawer.render());
+    if (!this.resourcedata)
+    {
+      var resptype = cls.ResourceUtil.mime_to_content_mode(this.resource.mime);
+      this.service.fetch_resource_data(this.on_resource_data.bind(this),
+                                       this.resource.id,
+                                       resptype);
+      // fixme: show progress thingy
+    }
+    else
+    {
+      var tpl = this.render_type_details(container, this.resource, this.resourcedata);
+      container.render(tpl);
+    }
+  }
+
+  this.render_type_details = function(container, resource, resourcedata)
+  {
+    return templates.resource_contents(resource, resourcedata)
+  }
+
+  this.on_resource_data = function(type, data)
+  {
+    const CONTENT = 5, TEXTCONTENT = 3;
+    this.resourcedata = data[CONTENT][TEXTCONTENT];
+    this.update();
+  }
+
+  this.init = function(res, service)
+  {
+    this.service = service;
+    this.resource = res;
+    this.resourcedata = null;
+    this.filename = cls.ResourceUtil.url_filename(res.url) || "<no name>";
+    this.drawer = new MetadataDrawer(res);
+    this.drawer.expanded = true;
+    cls.ResourceDetailBase.prototype.init.call(this, this.filename);
+  }
+}
+cls.ResourceDetailBase.prototype = new TempView();
+
 cls.GenericResourceDetail = function(res, service)
 {
-  this.resource = res;
-  this.filename = cls.ResourceUtil.url_filename(res.url) || "<no name>";
-  this.drawer = new MetadataDrawer(res);
-  this.drawer.expanded = true;
-  this.resourcedata = null;
-
-  this.createView = function(container)
-  {
-    this.container = container;
-    container.clearAndRender(this.drawer.render());
-    if (!this.resourcedata)
-    {
-      var resptype = cls.ResourceUtil.mime_to_content_mode(this.resource.mime);
-      service.fetch_resource_data(this.on_resource_data_bound,
-                                  this.resource.id,
-                                  resptype);
-      // fixme: show progress thingy
-    }
-    else
-    {
-      this.container.render(templates.resource_contents(this.resource, this.resourcedata));
-    }
-  };
-
-  this.on_resource_data_bound = function(type, data)
-  {
-    const CONTENT = 5, TEXTCONTENT = 3;
-    this.resourcedata = data[CONTENT][TEXTCONTENT];
-    this.update();
-  }.bind(this);
-
-  this.init(this.filename);
-};
-cls.GenericResourceDetail.prototype = new TempView();
-
-
-
-cls.FontResource = function(res, service)
-{
-  this.resource = res;
-  this.filename = cls.ResourceUtil.url_filename(res.url) || "<no name>";
-  this.drawer = new MetadataDrawer(res);
-  this.drawer.expanded = true;
-  this.resourcedata = null;
-
-  this.createView = function(container)
-  {
-    this.container = container;
-    container.clearAndRender(this.drawer.render());
-    if (!this.resourcedata)
-    {
-      var resptype = cls.ResourceUtil.mime_to_content_mode(this.resource.mime);
-      service.fetch_resource_data(this.on_resource_data_bound,
-                                  this.resource.id,
-                                  resptype);
-      // fixme: show progress thingy
-    }
-    else
-    {
-      this.container.render(templates.font_resource(this.resource, this.resourcedata));
-    }
-  };
-
-  this.on_resource_data_bound = function(type, data)
-  {
-    const CONTENT = 5, TEXTCONTENT = 3;
-    this.resourcedata = data[CONTENT][TEXTCONTENT];
-    this.update();
-  }.bind(this);
-
-  this.init(this.filename);
-};
-cls.FontResource.prototype = new TempView();
-
-
-
+  this.init(res, service);
+}
+cls.GenericResourceDetail.prototype = new cls.ResourceDetailBase();
 
 
 window.templates = window.templates || {};
@@ -131,6 +107,3 @@ window.templates.resource_contents = function(resource, data)
           "class", "padding resource-data-wrapper " + type
          ];
 }
-
-
-
