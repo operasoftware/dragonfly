e23705d424e4134ea8d787fe01b0a454b5f013ac
Rune Halvorsen <runeh@opera.com>
2011-03-23 18:01:02 +0100

    Visual improvements to metadata drawer

--- src/ui-scripts/metadata_drawer/metadata_drawer.js
+++ src/ui-scripts/metadata_drawer/metadata_drawer.js
@@ -5,7 +5,7 @@
   this._objectid = ObjectRegistry.get_instance().set_object(this);
   this._rules = {
     generic: [
-      {label: "url", getter: function(res) { return ["a", (res.urltype == 4 ? "data:URI" : res.url), "href", res.url, "target", "_blank"] } },
+      //{label: "url", getter: function(res) { return ["a", (res.urltype == 4 ? "data:URI" : res.url), "href", res.url, "target", "_blank"] } },
       {label: "size", getter: function(res) { return  res.size
                                               ? "" + window.helpers.pretty_print_number(res.size) + " bytes"
                                               : "-"
@@ -49,8 +49,10 @@
 
   this._toggle_handler = function(evt, target)
   {
-    var div = target.parentNode;
-    var obj = ObjectRegistry.get_instance().get_object(div.getAttribute("data-object-id"));
+    var objid = target.getAttribute('data-object-id');
+    var obj = ObjectRegistry.get_instance().get_object(objid);
+    var table = target.parentNode.parentNode.parentNode;
+    var div = table.parentNode;
     var container = div.parentNode;
     obj.toggle();
     div.re_render(obj.render());
@@ -70,24 +72,29 @@ templates.metadata_drawer = function(resource, expanded, objectid, rules, title)
   var content = [];
   if (expanded)
   {
-    content = templates.metadata_drawer_list(resource, rules);
-  }
-  else
-  {
-    content = "URL: " + url;
+    content = ["tr", ["td", templates.metadata_drawer_list(resource, rules), "colspan", "3"]]
   }
 
   return ["div",
-          ["input",
-           "handler", "metadata-drawer-toggle",
-           "class", expanded ? "unfolded" : "",
-           "unselectable", "on",
-           "type", "button",
-          ],
-          content,
-          (expanded && title ? ["h2", title] : []),
-          "class", "metadata-drawer " + (expanded ? "expanded" : "collapsed"),
-          "data-object-id", objectid,
+           ["table",
+             ["tr",
+               ["th",
+                 ["input",
+                  "handler", "metadata-drawer-toggle",
+                  "class", expanded ? "unfolded" : "",
+                  "unselectable", "on",
+                  "type", "button",
+                  "data-object-id", objectid
+                  ],
+                  "class", "expandercell" 
+               ],
+               ["th", templates.network_request_icon(resource), "class", "iconcell"],
+               ["th", url],
+             ],
+             "class", "metadata-drawer-summary-table"
+           ],
+           content,
+           "class", "metadata-drawer"
          ]
 }
 
