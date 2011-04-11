01c1d85b4f19ba5bb403d092897f0e39748752db
Rune Halvorsen <runeh@opera.com>
2011-04-11 13:25:39 +0200

    Check for failed requests in crafter. Fixes DFL-1827

--- src/ui-strings/ui_strings-en.js
+++ src/ui-strings/ui_strings-en.js
@@ -1220,3 +1220,7 @@ ui_strings.M_CONTEXTMENU_SHOW_RESOURCE = 'Show resource';
 
 /* DESC: Info text in the settings to invert the highlight color for elements. */
 ui_strings.S_INFO_INVERT_ELEMENT_HIGHLIGHT = "The element highlight color can be inverted with '%s' shortcut.";
+
+/* DESC: Info text in when a request in the request crafter failed. */
+ui_strings.S_INFO_REQUEST_FAILED = "The request failed.";
+
