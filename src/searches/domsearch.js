6906ad21e7705d300f27f834a4f7fd1c8e6e57bc
Chris K <chrisk@opera.com>
2011-05-30 18:30:21 +0200

    Code clean up.

--- src/searches/domsearch.js
+++ src/searches/domsearch.js
@@ -1,49 +1,46 @@
 var DOMSearch = function(min_length)
 {
+
+  /* inherits from TextSearch */
+
+  this._super_init = this._init;
+  this._super_highlight_next = this.highlight_next;
+  this._super_highlight_previous = this.highlight_previous;
+  this._super_update_search = this.update_search;
+  this._super_set_form_input = this.set_form_input;
+
+  /* interface */
+
+  this.highlight_next = function() {};
+
+  this.highlight_previous = function() {};
+  
+  this.update_search = function() {};
+
+  this.set_form_input = function() {};
+
+  this.search_delayed = function(event) {};
+
+  this.inspect_selected_node = function() {};
+
+  this.show_last_search = function() {};
+
+  this.clear_style_highlight_node = function() {};
+
   // TODO clean up onview destroy
   // TODO this.update_search, this.update (which one?), dependent on search type
 
-  this._init_super = this._init;
-
+  /* constants */
   const 
   TOKEN_HIGHLIGHT = [DOMSearch.PLAIN_TEXT, DOMSearch.REGEXP],
   MATCH_NODE_HIGHLIGHT_CLASS = "dom-search-match-cursor",
   NO_MATCH = TextSearch.NO_MATCH,
   EMPTY = TextSearch.EMPTY;
 
-  this._onsearchtypechange = function(event)
-  {
-    switch (event.target.name)
-    {
-      case 'dom-search-type':
-      {
-        this.search_type = parseInt(event.target.value);
-        this._set_highlight_handlers(event.target);
-        this._setting.set('dom-search-type', this.search_type);
-        this._validate_current_search();
-        break;
-      }
-      case 'dom-search-ignore-case':
-      {
-        this.ignore_case = Number(event.target.checked);
-        this._setting.set('dom-search-ignore-case', this.ignore_case);
-        this._validate_current_search();
-        break;
-      }
-      case 'dom-search-only-selected-node':
-      {
-        this.search_only_selected_node = Number(event.target.checked);
-        this._setting.set('dom-search-only-selected-node',
-                          this.search_only_selected_node);
-        this._validate_current_search();
-        break;
-      }
-    }
-  }.bind(this);
+  /* private */
 
   this._set_highlight_handlers = function(target)
   {
-
     if (TOKEN_HIGHLIGHT.indexOf(this.search_type) != -1)
     {
       this._initial_highlight = this._initial_highlight_token;
@@ -51,8 +48,8 @@ var DOMSearch = function(min_length)
       this._highlight_previous = this._highlight_previous_token;
       this._get_match_counts = this._get_match_counts_token;
       this._get_search_cursor = this._get_search_cursor_token;
-      this.update_search = this.update_search_token;
-      this.set_form_input = this.set_form_input_token;
+      this.update_search = this._update_search_token;
+      this.set_form_input = this._set_form_input_token;
       if (target)
       {
         target.form['dom-search-ignore-case'].disabled = false;
@@ -65,8 +62,8 @@ var DOMSearch = function(min_length)
       this._highlight_previous = this._highlight_previous_node;
       this._get_match_counts = this._get_match_counts_node;
       this._get_search_cursor = this._get_search_cursor_node;
-      this.update_search = this.update_search_node;
-      this.set_form_input = this.set_form_input_node;
+      this.update_search = this._update_search_node;
+      this.set_form_input = this._set_form_input_node;
       if (target)
       {
         target.form['dom-search-ignore-case'].disabled = true;
@@ -102,37 +99,21 @@ var DOMSearch = function(min_length)
     return this._match_cursor + 1;
   };
 
-  this.inspect_selected_node = function()
-  {
-    if (this._highligh_node)
-    {
-      this._broker.dispatch_action("dom", 
-                                   "inspect-node-link",
-                                   null,
-                                   this._highligh_node);
-    }
-  };
-
-  this.update_search_token = function()
+  this._update_search_token = function()
   {
     this._search_term = this._last_query;
-    this.update_search_super();
+    this._super_update_search();
   };
 
-  this.set_form_input_token = function(input)
+  this._set_form_input_token = function(input)
   {
     this._input = input;
     if (this._search_term)
     {
       this._input.value = this._orig_search_term;
       this._input.parentNode.firstChild.textContent = '';
-      this.update_search_token();
+      this._update_search_token();
     }
-  };
-
-  this.show_last_search = function()
-  {
-    this._handle_search();
   };
 
   /* methods for search type css and xpath */
@@ -153,15 +134,6 @@ var DOMSearch = function(min_length)
         return list;
       }, []);
       this._highlight_match_node(this._match_node_cursor < 0 ? 1 : 0);
-    }
-  };
-
-  this.clear_style_highlight_node = function()
-  {
-    if (this._highligh_node)
-    {
-      this._highligh_node.removeClass(MATCH_NODE_HIGHLIGHT_CLASS);
-      this._highligh_node = null;
     }
   };
 
@@ -214,26 +186,56 @@ var DOMSearch = function(min_length)
     return this._match_node_cursor + 1;
   };
 
-  this.update_search_node = function()
+  this._update_search_node = function()
   {
     this._initial_highlight_node();
   };
 
-  this.set_form_input_node = function(input)
+  this._set_form_input_node = function(input)
   {
     this._input = input;
     if (this._last_query)
     {
       this._input.value = this._last_query;
-      this.update_search_node();
+      this._update_search_node();
     }
   };
+
+  this._onsearchtypechange = function(event)
+  {
+    switch (event.target.name)
+    {
+      case 'dom-search-type':
+      {
+        this.search_type = parseInt(event.target.value);
+        this._set_highlight_handlers(event.target);
+        this._setting.set('dom-search-type', this.search_type);
+        this._validate_current_search();
+        break;
+      }
+      case 'dom-search-ignore-case':
+      {
+        this.ignore_case = Number(event.target.checked);
+        this._setting.set('dom-search-ignore-case', this.ignore_case);
+        this._validate_current_search();
+        break;
+      }
+      case 'dom-search-only-selected-node':
+      {
+        this.search_only_selected_node = Number(event.target.checked);
+        this._setting.set('dom-search-only-selected-node',
+                          this.search_only_selected_node);
+        this._validate_current_search();
+        break;
+      }
+    }
+  }.bind(this);
 
   this._onelementselected = function(msg)
   {
     this._selected_node = msg.obj_id;
     this._selected_runtime = msg.rt_id;
-  }
+  };
 
   this._onhighlightstyle = function(span_list)
   {
@@ -254,11 +256,12 @@ var DOMSearch = function(min_length)
 
   this._init = function(min_length)
   {
-    this._init_super(min_length);
+    this._super_init(min_length);
     this._setting = window.settings.dom;
     this.search_type = this._setting.get('dom-search-type');
     this.ignore_case = this._setting.get('dom-search-ignore-case');
-    this.search_only_selected_node = this._setting.get('dom-search-only-selected-node');
+    this.search_only_selected_node = this._setting
+                                     .get('dom-search-only-selected-node');
     this._min_term_length = 1;
     this._last_query = '';
     this._last_search_type = 0;
@@ -266,18 +269,13 @@ var DOMSearch = function(min_length)
     this._tagman = window.tag_manager;
     this._esdi = window.services['ecmascript-debugger'];
     this._broker = ActionBroker.get_instance();
-
-    window.eventHandlers.change['dom-search-type-changed'] = this._onsearchtypechange;
+    window.eventHandlers.change['dom-search-type-changed'] = 
+      this._onsearchtypechange;
     this._query_selector = ".dom-search-match";
     this._set_highlight_handlers();
     window.messages.addListener('element-selected', 
                                 this._onelementselected.bind(this));
   };
-
-
-
-  this._super_highlight_next = this.highlight_next;
-  this._super_highlight_previous = this.highlight_previous;
 
   this._validate_current_search = function()
   {
@@ -356,6 +354,8 @@ var DOMSearch = function(min_length)
     }
   }.bind(this);
 
+  /* implementation */
+
   this.highlight_next = function()
   {
     if (this._validate_current_search())
@@ -371,16 +371,11 @@ var DOMSearch = function(min_length)
       this._highlight_previous();
     }
   };
-
-  // handler for a view update
-  this.update_search_super = this.update_search;
   
   this.update_search = function()
   {
     // set depending on the search type in this._set_highlight_handlers
   };
-
-  this.set_form_input_super = this.set_form_input;
 
   this.set_form_input = function()
   {
@@ -396,6 +391,31 @@ var DOMSearch = function(min_length)
     else
     {
       this._validate_current_search();
+    }
+  };
+
+  this.inspect_selected_node = function()
+  {
+    if (this._highligh_node)
+    {
+      this._broker.dispatch_action("dom", 
+                                   "inspect-node-link",
+                                   null,
+                                   this._highligh_node);
+    }
+  };
+
+  this.show_last_search = function()
+  {
+    this._handle_search();
+  };
+
+  this.clear_style_highlight_node = function()
+  {
+    if (this._highligh_node)
+    {
+      this._highligh_node.removeClass(MATCH_NODE_HIGHLIGHT_CLASS);
+      this._highligh_node = null;
     }
   };
 
--- src/searches/domsearchview.js
+++ src/searches/domsearchview.js
@@ -3,13 +3,23 @@ window.cls || (window.cls = {});
 cls.DOMSearchView = function(id, name, container_class)
 {
 
+  this.createView = function(container)
+  {
+    container.clearAndRender(this._tmpl());
+    this._search_container = container.childNodes[1];
+    this._search.set_container(this._search_container);
+    var query = '[handler="' + this.controls[SEARCHFIELD].handler + '"]';
+    var input = container.querySelector(query);
+    this._search.set_form_input(input);
+    this._search.set_info_element(container.getElementsByTagName('info')[0]);
+    this._search.show_last_search();
+    input.focus();
+  };
+
   const 
-  MODE_SEARCHBAR = 1, 
-  MODE_SEARCHWINDOW = 2,
   MOVE_HIGHLIGHT_UP = 0,
   MOVE_HIGHLIGHT_DOWN = 1,  
-  SEARCHFIELD = 2,
-  SEARCH_MORE = 3;
+  SEARCHFIELD = 2;
 
   this._tmpl = function()
   {
@@ -26,25 +36,6 @@ cls.DOMSearchView = function(id, name, container_class)
         ['info', '\u00A0'],
         'class', 'dom-search-info'],
     ]);
-  };
-
-  this.createView = function(container)
-  {
-    container.clearAndRender(this._tmpl());
-    this._search_container = container.childNodes[1];
-    this._search.set_container(this._search_container);
-    var input = container.querySelector('[handler="' + 
-                                        this.controls[SEARCHFIELD].handler + 
-                                        '"]');
-    this._search.set_form_input(input);
-    this._search.set_info_element(container.getElementsByTagName('info')[0]);
-    this._search.show_last_search();
-    input.focus();
-  };
-
-  this.onresize = function()
-  {
-
   };
 
   ActionHandlerInterface.apply(this);
@@ -74,11 +65,8 @@ cls.DOMSearchView = function(id, name, container_class)
 
   this._onsearchfieldinput = function(event)
   {
-    // opera.postError(event.target.value)
     this._search.search_delayed();
-  }
-
-
+  };
 
   this._init = function(id, name, container_class)
   {
@@ -104,7 +92,6 @@ cls.DOMSearchView = function(id, name, container_class)
         title: ui_strings.S_INPUT_DEFAULT_TEXT_SEARCH
       },
     ];
-
     [
       'search_type',
       'ignore_case',
@@ -117,7 +104,6 @@ cls.DOMSearchView = function(id, name, container_class)
       });
       this.__defineSetter__(prop, function(){});
     }, this);
-
     eventHandlers.input[this.controls[SEARCHFIELD].handler] = 
       this._onsearchfieldinput.bind(this)
     eventHandlers.click[this.controls[MOVE_HIGHLIGHT_DOWN].handler] = 
@@ -126,16 +112,13 @@ cls.DOMSearchView = function(id, name, container_class)
       this._onshortcut.bind(this, 'highlight-previous-match');
     eventHandlers.mouseover['clear-style-highlight-node'] =
       this._search.clear_style_highlight_node.bind(this._search);
-    ActionBroker.get_instance().get_global_handler().
-    register_shortcut_listener(this.controls[SEARCHFIELD].shortcuts, 
-                               this._onshortcut.bind(this), 
-                               ['highlight-next-match',
-                                'highlight-previous-match',
-                                'hide-search']);
-    
+    ActionBroker.get_instance().get_global_handler()
+    .register_shortcut_listener(this.controls[SEARCHFIELD].shortcuts, 
+                                this._onshortcut.bind(this), 
+                                ['highlight-next-match',
+                                 'highlight-previous-match',
+                                 'hide-search']);
   };
-
-
 
   this._init(id, name, container_class);
 
