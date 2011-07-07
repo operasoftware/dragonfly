371846956cd1263920c8bf9fbfe4423ef193dc28
Rune Halvorsen <runeh@opera.com>
2011-07-07 16:42:16 +0200

    Experimental change to always enable request payloads

--- src/build-application/build_resource_manager_1_0.js
+++ src/build-application/build_resource_manager_1_0.js
@@ -8,5 +8,23 @@ window.app.builders.ResourceManager["1.0"] = function(service)
   new cls.NetworkLogView('network_logger', ui_strings.M_VIEW_LABEL_NETWORK_LOG, 'scroll', '', '');
   new cls.RequestCraftingView('request_crafter', ui_strings.M_VIEW_LABEL_REQUEST_CRAFTER, 'scroll', '', '');
   new cls.NetworkOptionsView('network_options', ui_strings.M_VIEW_LABEL_NETWORK_OPTIONS, 'scroll', '', '');
+
+  var setup_request_body_behaviour = function()
+  {
+    var text_types = ["text/html", "application/xhtml+xml", "application/mathml+xml",
+                     "application/xslt+xml", "text/xsl", "application/xml",
+                     "text/css", "text/plain", "application/x-javascript",
+                     "application/javascript", "text/javascript",
+                     "application/x-www-form-urlencoded"];
+
+    const STRING = 1, DECODE = 1;
+    var reqarg = [[STRING, DECODE],
+                 text_types.map(function(e) { return [e, [STRING, DECODE]]})
+                ];
+
+    window.services['resource-manager'].requestSetRequestMode(null, reqarg);
+  }
+
+  window.services['resource-manager'].addListener('enable-success', setup_request_body_behaviour);
   return true;
 };
