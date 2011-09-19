ce400fd5a858723cf3b340bff77aece6dfaaee80
David Håsäther <davidh@opera.com>
2011-09-19 14:45:16 +0200

    Removed unused variable.

--- src/http-logger/httplogger_view.js
+++ src/http-logger/httplogger_view.js
@@ -192,10 +192,8 @@ cls.HttpLogger["2.0"].RequestListView = function(id, name, container_class)
      */
     this.toggleDetails = function(id)
     {
-        var dirty = false;
         if (expandedItems.indexOf(id) == -1) {
             expandedItems.push(id);
-            dirty = true;
         } else {
             expandedItems.splice(expandedItems.indexOf(id), 1);
         }
