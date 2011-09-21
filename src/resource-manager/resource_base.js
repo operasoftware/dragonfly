c0984d3d10a05d5d6f7ef6d72f8791522e48cc36
Arve Bersvendsen <arveb@opera.com>
2011-09-21 15:29:48 +0200

    Updated template to add appropriate classes to avoid throwing

--- src/resource-manager/resource_base.js
+++ src/resource-manager/resource_base.js
@@ -108,10 +108,10 @@ cls.ResourceDetailBase = function()
 
     if (!data || !(data.lines && data.lines[0])) return;
     this._root_ele = container.getElementsByClassName(RESOURCE_DETAIL_CONTAINER_CLASSNAME)[0];
-    this.clear_line_numbers(this._root_ele)
     this._line = parseInt(data.lines[0]);
     if (this._root_ele)
     {
+      this.clear_line_numbers(this._root_ele)
       this._current_line = 1;
       this._traverse_ele(this._root_ele);
     }
@@ -193,7 +193,9 @@ cls.GenericResourceDetail = function(res, service)
 {
   this.render_type_details = function(container, resource, resourcedata)
   {
-    return ["h1", "Don't know what this resource is"];
+    return ["div",
+            ["h1", "Don't know what this resource is"],
+            "class", "resource-detail-container"];
   }
 
   this.init(res, service);
