5441a4e2f6f789aeb38d58b53ab0243c506946fe
Chris K <chrisk@opera.com>
2011-10-12 15:02:12 +0200

    Code style fix.

--- src/repl/propertyfinder.js
+++ src/repl/propertyfinder.js
@@ -33,13 +33,17 @@ window.cls.PropertyFinder = function(rt_id) {
   this._get_last_punctuator = function(tokens, punctuator)
   {
     if (tokens)
+    {
       for (var i = tokens.length - 1, token; token = tokens[i]; i--)
+      {
         if (token[TYPE] == PUNCTUATOR && token[VALUE] == punctuator)
         {
           for (var j = 0, index = 0; j < i; j++)
             index += tokens[j][VALUE].length;
           return index;
         }
+      }
+    }
     return -1;
   };
 
