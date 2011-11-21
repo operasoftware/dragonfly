c72c184c409085a700f76dcd53dc6c36696fec63
Chris K <chrisk@opera.com>
2011-11-21 18:48:19 +0100

    Fix hide tooltip.

--- src/ui-scripts/tooltip/tooltip.js
+++ src/ui-scripts/tooltip/tooltip.js
@@ -142,6 +142,7 @@
           _tooltip_ele_first_child.appendChild(_current_tooltip._container);
         }
 
+        _current_tooltip._accept_call = true;
         _last_handler_ele = ele;
         _last_box = ele.getBoundingClientRect();
         _last_event = event;
@@ -152,6 +153,9 @@
       ele = ele.parentNode;
     }
 
+    if (_current_tooltip)
+      _current_tooltip._accept_call = false;
+    
     _set_hide_timeout();
   };
 
@@ -204,7 +208,7 @@
 
   var _show_tooltip = function(tooltip, content, box)
   {
-    if (tooltip == _current_tooltip)
+    if (tooltip == _current_tooltip && _current_tooltip._accept_call)
     {
       if (!content && _last_handler_ele)
         content = _last_handler_ele.getAttribute(DATA_TOOLTIP_TEXT);
@@ -268,8 +272,11 @@
 
   var _hide_tooltip = function(tooltip)
   {
-    if (tooltip == _current_tooltip)
-      _hide_tooltip();
+    if (tooltip == _current_tooltip && _current_tooltip._accept_call)
+    {
+      _clear_show_timeout();
+      _tooltip_ele.style.cssText = "";
+    }
   };
 
   var _on_tooltip_enter = function(event)
