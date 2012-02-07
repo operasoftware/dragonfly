cf957573fc4dcb3925a976ce3b8b8dfea8732fec
Chris K <chrisk@opera.com>
2012-02-07 18:47:05 +0100

    Fixed the URI class.

--- src/scripts/uri.js
+++ src/scripts/uri.js
@@ -10,16 +10,12 @@ var URIPrototype = function(uri_prop_name)
     
     hash
     host
-    hostname
-    href
     pathname
-    port
     protocol
     search
     filename
-    dir_pathname  pathname minus filename
-    abs_dir protocol plus hostname plus dir_pathname
-
+    dir_pathname // pathname minus filename
+    abs_dir // protocol plus hostname plus dir_pathname
   */
 
   [
@@ -35,10 +31,10 @@ var URIPrototype = function(uri_prop_name)
   {
     this.__defineGetter__(prop, function()
     {
-      if (!this._a)
+      if (!this._is_parsed)
         this._init();
 
-      return this._a ? this._a[prop] : undefined;  
+      return this["_" + prop];  
     });
     this.__defineSetter__(prop, function() {});
     
@@ -47,8 +43,14 @@ var URIPrototype = function(uri_prop_name)
   this.__defineGetter__("filename", function()
   {
         
-    if (!this._filename && (this._a || this[uri_prop_name]))
-      this._filename = this.pathname.slice(this.pathname.lastIndexOf("/") + 1);
+    if (!this._filename && (this._is_parsed || this[uri_prop_name]))
+    {
+      var pos = this.pathname.lastIndexOf("/");
+      if (pos > -1)
+        this._filename = this.pathname.slice(pos + 1);
+      else
+        this._filename = this.pathname;
+    }
 
     return this._filename;  
   });
@@ -57,8 +59,14 @@ var URIPrototype = function(uri_prop_name)
 
   this.__defineGetter__("dir_pathname", function()
   {
-    if (!this._dir_pathname && (this._a || this[uri_prop_name]))
-      this._dir_pathname = this.pathname.slice(0, this.pathname.lastIndexOf("/") + 1);
+    if (!this._dir_pathname && (this._is_parsed || this[uri_prop_name]))
+    {
+      var pos = this.pathname.lastIndexOf("/");
+      if (pos > -1)
+        this._dir_pathname = this.pathname.slice(0, pos + 1);
+      else
+        this._dir_pathname = "";
+    }
 
     return this._dir_pathname;  
   });
@@ -67,8 +75,9 @@ var URIPrototype = function(uri_prop_name)
 
   this.__defineGetter__("abs_dir", function()
   {
-    if (!this._abs_dir && (this._a || this[uri_prop_name]))
-      this._abs_dir = this.protocol + "//" + this.host + this.dir_pathname;
+    if (!this._abs_dir && (this._is_parsed || this[uri_prop_name]))
+      this._abs_dir = (this.protocol ? this.protocol + "//": "") +
+                      this.host + this.dir_pathname;
 
     return this._abs_dir;  
   });
@@ -77,7 +86,7 @@ var URIPrototype = function(uri_prop_name)
 
   this.__defineGetter__("origin", function()
   {
-    if (!this._origin && (this._a || this[uri_prop_name]))
+    if (!this._origin && (this._is_parsed || this[uri_prop_name]))
       this._origin = this.protocol + "//" + this.host;
 
     return this._origin;  
@@ -92,9 +101,57 @@ var URIPrototype = function(uri_prop_name)
 
     if (uri)
     {
-      this._a = document.createElement("a");
-      this._a.href = uri;
+      var val = uri;
+
+      var pos = val.indexOf("#");
+      if (pos > -1)
+      {
+        this._hash = val.slice(pos);
+        val = val.slice(0, pos);
+      }
+      else
+        this._hash = "";
+
+      pos = val.indexOf("?");
+      if (pos > -1)
+      {
+        this._search = val.slice(pos);
+        val = val.slice(0, pos);
+      }
+      else
+        this._search = "";
+
+      pos = val.indexOf(":");
+      if (pos > -1)
+      {
+        this._protocol = val.slice(0, pos + 1);
+        val = val.slice(pos + 1);
+        while (val.indexOf("/") === 0)
+          val = val.slice(1);
+      }
+      else
+        this._protocol = "";
+
+      pos = val.indexOf("/");
+      if (pos > -1)
+      {
+        this._host = val.slice(0, pos);
+        val = val.slice(pos);
+      }
+      else if (this._protocol)
+      {
+        this._host = val
+      }
+      else
+        this._host = "";
+
+      if (val)
+        this._pathname = val;
+      else
+        this._pathname = "";
     }
+
+    this._is_parsed = true;
   };
 
 };
