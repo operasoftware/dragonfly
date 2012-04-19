89b5fa1560253b5b3d15e53156f7777c6a8a1364
Chris K <chrisk@opera.com>
2012-04-19 17:03:17 +0200

    Disabled view for JS source.

--- src/ecma-debugger/runtimes.js
+++ src/ecma-debugger/runtimes.js
@@ -170,7 +170,20 @@ cls.EcmascriptDebugger["6.0"].Runtimes = function(service_version)
   var _on_profile_disabled = function(msg)
   {
     if (msg.profile == window.app.profiles.DEFAULT)
-      onResetState();
+    {
+      __runtimes = {};
+      __old_runtimes = {};
+      __runtimes_arr = []; // runtime ids
+      __window_ids = {};
+      __windows_reloaded = {};
+      __threads = [];
+      __log_threads = false;
+      __windowsFolding = {};
+      __selected_runtime_id = '';
+      __next_runtime_id_to_select = '';
+      __selected_script = '';
+      updateRuntimeViews();
+    }
   };
 
   var _on_profile_enabled = function(msg)
