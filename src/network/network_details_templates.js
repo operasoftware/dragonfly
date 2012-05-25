6a456515f93cc832e74c91f7f3e25b3a77c793d8
Daniel Herzog <dherzog@opera.com>
2012-05-25 09:24:22 +0200

    Small readability fixes

--- src/network/network_details_templates.js
+++ src/network/network_details_templates.js
@@ -31,36 +31,38 @@ templates.network_log_details = function(ctx, selected, left_val)
 templates.network_log_detail = function(ctx, selected)
 {
   var entry = ctx.get_entry_from_filtered(selected);
-  var responsecode = entry.responses.length && entry.responses.last.responsecode;
+  var responsecode = entry.responses.last && entry.responses.last.responsecode;
   if (responsecode && responsecode in cls.ResourceUtil.http_status_codes)
      responsecode = responsecode + " " + cls.ResourceUtil.http_status_codes[responsecode];
 
-  return ["div",
-    ["table",
-      ["tbody",
-        ["tr",
-          ["th", ui_strings.S_HTTP_LABEL_URL + ":"], ["td", entry.human_url]
-        ],
-        ["tr",
-          ["th", ui_strings.S_HTTP_LABEL_METHOD + ":"],
-          ["td", entry.touched_network ? entry.method : ui_strings.S_RESOURCE_ALL_NOT_APPLICABLE],
-          "data-spec", "http#" + entry.method
-        ],
-        ["tr",
-          ["th", ui_strings.M_NETWORK_REQUEST_DETAIL_STATUS + ":"],
-          ["td",
-            entry.touched_network && responsecode ? String(responsecode) : ui_strings.S_RESOURCE_ALL_NOT_APPLICABLE
+  return (
+    ["div",
+      ["table",
+        ["tbody",
+          ["tr",
+            ["th", ui_strings.S_HTTP_LABEL_URL + ":"], ["td", entry.human_url]
           ],
-         "data-spec", "http#" + entry.responsecode
-        ]
+          ["tr",
+            ["th", ui_strings.S_HTTP_LABEL_METHOD + ":"],
+            ["td", entry.touched_network ? entry.method : ui_strings.S_RESOURCE_ALL_NOT_APPLICABLE],
+            "data-spec", "http#" + entry.method
+          ],
+          ["tr",
+            ["th", ui_strings.M_NETWORK_REQUEST_DETAIL_STATUS + ":"],
+            ["td",
+              entry.touched_network && responsecode ? String(responsecode) : ui_strings.S_RESOURCE_ALL_NOT_APPLICABLE
+            ],
+           "data-spec", "http#" + entry.responsecode
+          ]
+        ],
+        templates.request_details(entry),
+        templates.network_request_body(entry),
+        entry.touched_network ? entry.responses.map(templates.network_response) : []
       ],
-      templates.request_details(entry),
-      templates.network_request_body(entry),
-      entry.touched_network ? entry.responses.map(templates.network_response) : []
-    ],
-    "data-object-id", String(entry.id),
-    "class", "request-details"
-  ];
+      "data-object-id", String(entry.id),
+      "class", "request-details"
+    ]
+  );
 };
 
 templates.network_response = function(response)
