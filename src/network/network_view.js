4cfee5ac95331e597bfc5b43066f045ad271bd7a
Daniel Herzog <dherzog@opera.com>
2012-05-31 13:54:20 +0200

    Partial fix for DFL-3154: Method column does not have a title, sorts weirdly

--- src/network/network_view.js
+++ src/network/network_view.js
@@ -216,11 +216,7 @@ cls.NetworkLogView = function(id, name, container_class, html, default_handler)
     idgetter: function(res) { return String(res.id) },
     columns: {
       method: {
-        label: ui_strings.S_HTTP_LABEL_METHOD,
-        headerlabel: "",
-        renderer: function(entry) {
-          return entry.method;
-        }
+        label: ui_strings.S_HTTP_LABEL_METHOD
       },
       responsecode: {
         label: ui_strings.S_HTTP_LABEL_RESPONSECODE,
