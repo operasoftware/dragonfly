1f750f864e818aca32eefc4ec53f6f798745cf2f
Daniel Herzog <dherzog@opera.com>
2012-06-01 12:37:09 +0200

    Always show the actual url in the network details, even for data uris

--- src/network/network_details_templates.js
+++ src/network/network_details_templates.js
@@ -39,7 +39,7 @@ templates.network_log_detail = function(entry)
       ["table",
         ["tbody",
           ["tr",
-            ["th", ui_strings.S_HTTP_LABEL_URL + ":"], ["td", entry.human_url]
+            ["th", ui_strings.S_HTTP_LABEL_URL + ":"], ["td", entry.url]
           ],
           ["tr",
             ["th", ui_strings.S_HTTP_LABEL_METHOD + ":"],
