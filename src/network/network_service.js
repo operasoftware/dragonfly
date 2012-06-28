905da3b8b3ee097d7e846b5c89f2864cbf3948ef
Daniel Herzog <dherzog@opera.com>
2012-06-28 18:24:40 +0200

    Fixed error_in_last_responsecode

--- src/network/network_service.js
+++ src/network/network_service.js
@@ -763,7 +763,7 @@ cls.NetworkLoggerEntryPrototype = function()
   this._update_event_response = function(event)
   {
     this.last_responsecode = event.responseCode;
-    this.error_in_last_response = /^[45]/.test(this.responsecode);
+    this.error_in_last_response = /^[45]/.test(this.last_responsecode);
     this._current_response = new cls.NetworkLoggerResponse(this);
     this.requests_responses.push(this._current_response);
     this._current_response._update_event_response(event);
