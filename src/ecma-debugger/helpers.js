6648ef5e12435435a0e79be63c3ec842d79c7745
David Håsäther <davidh@opera.com>
2012-07-03 15:08:36 +0200

    Use a map (object) instead of an array, for readability.

--- src/ecma-debugger/helpers.js
+++ src/ecma-debugger/helpers.js
@@ -139,21 +139,42 @@ window.cls.Helpers = function()
    */
   this.escape_input = (function()
   {
-    var regexps = [
-      [/\\/g, "\\\\"],
-      [/"/g, "\\\""],
-      [/'/g, "\\'"],
-      [/\n/g, "\\n"],
-      [/\r/g, "\\r"],
-      [/\u2028/g, "\\u2028"],
-      [/\u2029/g, "\\u2029"]
+    var replacement_map = [
+      {
+        regexp: /\\/g,
+        replacement: "\\\\"
+      },
+      {
+        regexp: /"/g,
+        replacement: "\\\""
+      },
+      {
+        regexp: /'/g,
+        replacement: "\\'"
+      },
+      {
+        regexp: /\n/g,
+        replacement: "\\n"
+      },
+      {
+        regexp: /\r/g,
+        replacement: "\\r"
+      },
+      {
+        regexp: /\u2028/g,
+        replacement: "\\u2028"
+      },
+      {
+        regexp: /\u2029/g,
+        replacement: "\\u2029"
+      }
     ];
 
     return function escape_input(str)
     {
-      for (var i = 0, re; re = regexps[i]; i++)
+      for (var i = 0, re; re = replacement_map[i]; i++)
       {
-        str = str.replace(re[0], re[1]);
+        str = str.replace(re.regexp, re.replacement);
       }
       return str;
     }
