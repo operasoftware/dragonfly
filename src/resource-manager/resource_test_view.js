e371ac62017fdabe63a639c1e30cbb8a0b618851
p01 <p01@opera.com>
2012-07-06 13:36:55 +0200

    Added missing reload-window class

--- src/resource-manager/resource_test_view.js
+++ src/resource-manager/resource_test_view.js
@@ -64,7 +64,7 @@ cls.ResourceTreeView = function(id, name, container_class, html, default_handler
       container.clearAndRender(
         ['div',
          ['span',
-          'class', 'container-button ui-button',
+          'class', 'container-button ui-button reload-window',
           'handler', 'reload-window',
           'tabindex', '1'],
          ['p', ui_strings.S_RESOURCE_CLICK_BUTTON_TO_FETCH_RESOURCES],
