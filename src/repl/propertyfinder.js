5e26ee3628be9264feb1c8ef680369a33511f2df
Chris K <chrisk@opera.com>
2012-07-09 09:20:48 +0200

    Additional fix for DFL-3310, added some tests and fixed a regression.

--- src/repl/propertyfinder.js
+++ src/repl/propertyfinder.js
@@ -61,32 +61,29 @@ window.cls.PropertyFinder = function(rt_id) {
   this._find_input_parts = function(input)
   {
     var tokens = [];
-
     this._parser.tokenize(input, function(token_type, token)
     {
       tokens.push([token_type, token]);
     });
-
     var last_bracket = this._get_last_token(tokens, PUNCTUATOR, '[');
     var last_brace = this._get_last_token(tokens, PUNCTUATOR, '(');
-
     last_brace = this._get_last_token(tokens, PUNCTUATOR, ')') <= last_brace
                ? last_brace
                : -1;
     last_bracket = this._get_last_token(tokens, PUNCTUATOR, ']') <= last_bracket
                  ? last_bracket
                  : -1;
-    input = input.slice(Math.max(last_brace,
-                                 last_bracket,
-                                 this._get_last_token(tokens, PUNCTUATOR, '='),
-                                 this._get_last_token(tokens, IDENTIFIER, 'in')));
+    var pos = Math.max(last_brace,
+                       last_bracket,
+                       this._get_last_token(tokens, PUNCTUATOR, '='),
+                       this._get_last_token(tokens, IDENTIFIER, 'in'));
+    if (pos > -1)
+      input = input.slice(pos);
     input = input.replace(/^\s+/, '');
-
     var last_dot = input.lastIndexOf('.');
     var new_path = '';
     var new_id = '';
     var ret = '';
-
     if(last_dot > -1)
     {
       new_path = input.slice(0, last_dot);
@@ -96,7 +93,6 @@ window.cls.PropertyFinder = function(rt_id) {
     {
       new_id = input;
     }
-
     return {scope: new_path, identifier: new_id, input: input};
   };
 
