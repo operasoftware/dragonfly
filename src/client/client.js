f020c60693b8b9aebcb2da930e65fb02751c1ea3
p01 <p01@opera.com>
2012-07-11 15:57:59 +0200

    added min_width on the resource_tree_view

--- src/client/client.js
+++ src/client/client.js
@@ -610,11 +610,12 @@ ui_framework.layouts.resource_rough_layout =
   [
     {
       name: 'resource_tree_panel',
-      width: 500, tabbar: { id:'resources_tree', tabs: ['resource_tree_view'], is_hidden:true }
+      min_width:150,
+      width: 300, tabbar: { id:'resources_tree', tabs: ['resource_tree_view'], is_hidden:true }
     },
     {
       name: 'resource_detail_panel',
-      width:400, tabbar:{id:'resource_detail',tabs:['resource_detail_view'], is_hidden:true }
+      width:600, tabbar:{id:'resource_detail',tabs:['resource_detail_view'], is_hidden:true }
     }
   ]
 };
