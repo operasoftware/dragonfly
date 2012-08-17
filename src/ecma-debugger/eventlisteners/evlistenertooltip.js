9f87680c6b7d6320243402c170609b5f3534c451
Chris K <chrisk@opera.com>
2012-08-17 11:54:16 +0200

    Simplified signature of ConfirmDialog.

--- src/ecma-debugger/eventlisteners/evlistenertooltip.js
+++ src/ecma-debugger/eventlisteners/evlistenertooltip.js
@@ -49,10 +49,8 @@ cls.EventListenerTooltip = function()
   var _reload_script_dialog = function(event, target)
   {
     new ConfirmDialog(ui_strings.D_RELOAD_SCRIPTS,
-                      function() { window.runtimes.reloadWindow(); },
-                      null,
-                      ui_strings.S_BUTTON_YES,
-                      ui_strings.S_BUTTON_NO).show();
+                      [function() { window.runtimes.reloadWindow(); }],
+                      ConfirmDialog.YES_NO).show();
   };
 
   var _init = function(view)
