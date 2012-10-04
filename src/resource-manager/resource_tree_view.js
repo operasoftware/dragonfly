a578f0b98d0d3d3946a144b8354f2473e4f6d07d
p01 <p01@opera.com>
2012-10-04 15:09:02 +0200

    Preserve the scollLeft position

--- src/resource-manager/resource_tree_view.js
+++ src/resource-manager/resource_tree_view.js
@@ -28,6 +28,7 @@ cls.ResourceTreeView = function(id, name, container_class, html, default_handler
     var service = this._service;
     var ctx = this._service.get_resource_context();
     var scrollTop = container.firstElementChild?container.firstElementChild.scrollTop:0;
+    var scrollLeft = container.firstElementChild?container.firstElementChild.scrollLeft:0;
 
     if (ctx )
     {
@@ -58,6 +59,7 @@ cls.ResourceTreeView = function(id, name, container_class, html, default_handler
     }
 
     container.firstElementChild.scrollTop = scrollTop;
+    container.firstElementChild.scrollLeft = scrollLeft;
 	};
 
   this.create_disabled_view = function(container)
