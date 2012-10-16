290979b24580d890909a951844d94f1d6f6c41a4
p01 <p01@opera.com>
2012-10-16 13:02:35 +0200

    Added a note about throttled methods.

--- src/lib/functionprototype.js
+++ src/lib/functionprototype.js
@@ -1,6 +1,11 @@
 ﻿/**
  * Throttle the function to run at most every delay ms
  * @argument delay {Number}
+ *
+ * Note: Throttled methods are argument-less because we don't know which set of
+ * arguments ( among the ones passed to all the throttled calls ) to pass to the
+ * method it is eventually called. Instead they should use a state or context
+ * variable of their scope.
  */
 Function.prototype.throttle || (Function.prototype.throttle = function(delay)
 {
