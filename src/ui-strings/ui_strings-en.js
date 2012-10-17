bb465839968020a51e999ca1bbd27268f5f4276d
Daniel Herzog <dherzog@opera.com>
2012-10-17 15:31:18 +0200

    Ran cleanrepo

--- src/ui-strings/ui_strings-en.js
+++ src/ui-strings/ui_strings-en.js
@@ -1,4 +1,4 @@
-window.ui_strings || ( window.ui_strings  = {} );
+﻿window.ui_strings || ( window.ui_strings  = {} );
 window.ui_strings.lang_code = "en";
 
 /**
@@ -1802,3 +1802,4 @@ ui_strings.S_TOGGLE_PAUSED_UPDATING_NETWORK_VIEW = "Pause updating network activ
 
 /* DESC: String shown instead of filename when file name is missing */
 ui_strings.S_UNKNOWN_SCRIPT = "(Unknown script)";
+
