84f8077779da1779ee97eefcc13d6f87c42f8701
p01 <p01@opera.com>
2012-11-02 15:13:20 +0100

    Fixed infinite search-createView loop and cleaned up the throttling

--- src/resource-manager/resource_tree_view.js
+++ src/resource-manager/resource_tree_view.js
@@ -19,6 +19,7 @@ cls.ResourceTreeView = function(id, name, container_class, html, default_handler
     ui_strings.S_HTTP_LABEL_FILTER_OTHER
   ];
 
+  this._next_render_time = 0;
   this._resource_inspector = resource_inspector;
   this.required_services = ["resource-manager", "document-manager"];
 
@@ -27,10 +28,31 @@ cls.ResourceTreeView = function(id, name, container_class, html, default_handler
     return GROUP_ORDER;
   };
 
-  // throttle the update
-  this.update = this.update.bind(this).throttle(THROTTLE_DELAY);
+  this.instant_update = function()
+  {
+    // Reset the _next_render_time to force an immediate render
+    this._next_render_time = 0;
+    this.update();
+  };
+
+  this.update_bound = this.update.bind(this);
 
   this.createView = function(container)
+  {
+    var now = Date.now();
+    if (now >= this._next_render_time)
+    {
+      if (this._bounced_update)
+        this._bounced_update = clearTimeout(this._bounced_update);
+
+      this._render_view(container);
+      this._next_render_time = now + THROTTLE_DELAY;
+    }
+    else if (!this._bounced_update)
+      this._bounced_update = setTimeout(this.update_bound, THROTTLE_DELAY);
+	};
+
+  this._render_view = function(container)
   {
     var tpl;
     var ctx = this._resource_inspector.get_resource_context();
@@ -73,11 +95,11 @@ cls.ResourceTreeView = function(id, name, container_class, html, default_handler
       target.scrollTop = scroll_top;
       target.scrollLeft = scroll_left;
     }
-	};
+  };
 
   this.ondestroy = function(container)
   {
-    delete this.tpl_JSON;
+    this.tpl_JSON = null;
   };
 
   this.create_disabled_view = function(container)
@@ -90,11 +112,11 @@ cls.ResourceTreeView = function(id, name, container_class, html, default_handler
     this.id = id;
 
     var messages = window.messages;
-    messages.add_listener("debug-context-selected", this.update);
+    messages.add_listener("debug-context-selected", this.update_bound);
 
     var doc_service = window.services["document-manager"];
-    doc_service.add_listener("abouttoloaddocument", this.update);
-    doc_service.add_listener("documentloaded", this.update);
+    doc_service.add_listener("abouttoloaddocument", this.update_bound);
+    doc_service.add_listener("documentloaded", this.update_bound);
 
     ActionHandlerInterface.apply(this);
     this._handlers = {
@@ -137,11 +159,8 @@ cls.ResourceTreeView.create_ui_widgets = function()
   text_search.add_listener("onbeforesearch", (function(msg)
   {
     var view = window.views.resource_tree_view;
-    if (view.search_term != msg.search_term)
-    {
-      view.search_term = msg.search_term;
-      view.update();
-    }
+    view.search_term = msg.search_term;
+    view.instant_update();
   }).bind(text_search));
 
   window.event_handlers.input["resource-tree-text-search"] = function(event, target)
@@ -156,17 +175,10 @@ cls.ResourceTreeView.create_ui_widgets = function()
 
   var on_view_created = function(msg)
   {
-    if (msg.id == "resource_tree_view")
+    if (msg.id == "resource_tree_view" && msg.container)
     {
-      if (msg.container)
-      {
-        text_search.setContainer(msg.container);
-        text_search.set_query_selector(".resource-tree-resource-label");
-        text_search.setFormInput(
-          views.resource_tree_view.getToolbarControl(msg.container, "resource-tree-text-search")
-        );
-        window.views.resource_tree_view.search_term = "";
-      }
+      text_search.setContainer(msg.container);
+      text_search.set_query_selector(".resource-tree-resource-label");
     }
   };
 
@@ -181,6 +193,6 @@ cls.ResourceTreeView.create_ui_widgets = function()
 
   window.messages.add_listener("view-created", on_view_created);
   window.messages.add_listener("view-destroyed", on_view_destroyed);
-}
+};
 
 cls.ResourceTreeView.prototype = ViewBase;
