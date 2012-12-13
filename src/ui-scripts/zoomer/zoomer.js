d7cc8d0a7977a49c7627a03b9a004d2b64f2a105
David Håsäther <hasather@gmail.com>
2012-12-13 13:59:55 +0100

    Check for event.key instead of event.which in keydown events.

--- src/ui-scripts/zoomer/zoomer.js
+++ src/ui-scripts/zoomer/zoomer.js
@@ -207,20 +207,23 @@ var ZoomerPrototype = function()
     if (this._handle_ele)
       return;
 
+    if (!event.key)
+      return;
+
     var width = this._to_right_x(this._overlay_right) - this._overlay_left;
     var diff = {
-      33: width,  // Page up
-      34: -width, // Page down
-      37: -KEYDOWN_DIFF, // Arrow left
-      39: KEYDOWN_DIFF   // Arrow right
-    }[event.which];
+      "PageUp": width,
+      "PageDown": -width,
+      "Left": -KEYDOWN_DIFF,
+      "Right": KEYDOWN_DIFF
+    }[event.key];
     if (diff)
       this.move_overlay(diff);
 
     diff = {
-      38: -KEYDOWN_DIFF, // Arrow up
-      40: KEYDOWN_DIFF   // Arrow down
-    }[event.which];
+      "Up": -KEYDOWN_DIFF,
+      "Down": KEYDOWN_DIFF
+    }[event.key];
     if (diff)
       this.change_overlay_size(-diff, diff);
 
@@ -287,10 +290,14 @@ var ZoomerPrototype = function()
 
   this._handle_ele_onkeydown = function(event)
   {
+    if (!event.key)
+      return;
+
     var diff = {
-      37: -KEYDOWN_DIFF, // Arrow left
-      39: KEYDOWN_DIFF   // Arrow right
-    }[event.which];
+      "Left": -KEYDOWN_DIFF,
+      "Right": KEYDOWN_DIFF
+    }[event.key];
+
     if (!diff)
       return;
     var is_left_handle = this._handle_ele == this._handle_left_ele;
